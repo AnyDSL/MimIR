6f1d0ba3163a1c7649245f52bbcdbb109147959e
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2021-11-03 12:50:56 +0100

    merge closure conv. (f-fries/thorin/tree/t2)

--- src/thorin/transform/closure_conv.h
+++ src/thorin/transform/closure_conv.h
@@ -15,7 +15,7 @@ class FVA {
 public:
     FVA(World& world)
         : world_(world)
-        , cur_pass_id(1) 
+        , cur_pass_id(1)
         , lam2nodes_() {};
 
     DefSet& run(Lam *lam);
@@ -26,7 +26,7 @@ private:
     using Nodes = std::vector<Node*>;
 
     struct Node {
-        Lam *lam;
+        Def *nom;
         DefSet fvs;
         Nodes preds;
         Nodes succs;
@@ -34,14 +34,14 @@ private:
     };
 
     bool is_bot(Node* node) { return node->pass_id == 0; }
-    bool is_done(Node* node) { 
-        return !is_bot(node) && node->pass_id < cur_pass_id; 
+    bool is_done(Node* node) {
+        return !is_bot(node) && node->pass_id < cur_pass_id;
     }
     void mark(Node* node) { node->pass_id = cur_pass_id; }
 
-    void split_fv(const Def* fv, DefSet& out);
+    void split_fv(Def *nom, const Def* fv, DefSet& out);
 
-    std::pair<Node*, bool> build_node(Lam* lam, NodeQueue& worklist);
+    std::pair<Node*, bool> build_node(Def* nom, NodeQueue& worklist);
     void run(NodeQueue& worklist);
 
     World& world() { return world_; }
@@ -52,38 +52,38 @@ private:
 };
 
 class ClosureConv {
-    public:
-        ClosureConv(World& world)
-            : world_(world)
-            , fva_(world)
-            , closures_(DefMap<Closure>())
-            , closure_types_(Def2Def())
-            , worklist_(std::queue<const Def*>()) {};
+public:
+    ClosureConv(World& world)
+        : world_(world)
+        , fva_(world)
+        , closures_(DefMap<Closure>())
+        , closure_types_(Def2Def())
+        , worklist_(std::queue<const Def*>()) {};
 
-        void run();
+    void run();
 
-    private:
-        struct Closure {
-            Lam* old_fn;
-            size_t num_fvs;
-            const Def* env;
-            Lam* fn;
-        };
+private:
+    struct Closure {
+        Lam* old_fn;
+        size_t num_fvs;
+        const Def* env;
+        Lam* fn;
+    };
 
 
-        const Def* rewrite(const Def* old_def, Def2Def& subst);
+    const Def* rewrite(const Def* old_def, Def2Def& subst);
 
-        const Def* closure_type(const Pi* pi, Def2Def& subst, const Def* ent_type = nullptr);
+    const Def* closure_type(const Pi* pi, Def2Def& subst, const Def* ent_type = nullptr);
 
-        Closure make_closure(Lam* lam, Def2Def& subst);
+    Closure make_closure(Lam* lam, Def2Def& subst);
 
-        World& world() { return world_; }
+    World& world() { return world_; }
 
-        World& world_;
-        FVA fva_;
-        DefMap<Closure> closures_;
-        Def2Def closure_types_;
-        std::queue<const Def*> worklist_;
+    World& world_;
+    FVA fva_;
+    DefMap<Closure> closures_;
+    Def2Def closure_types_;
+    std::queue<const Def*> worklist_;
 
 };
 
