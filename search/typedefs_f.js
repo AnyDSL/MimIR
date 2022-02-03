d33c2d7bbcb2b4806369b53742a01966be7ca1fb
leissa <leissa@users.noreply.github.com>
2022-02-03 18:02:14 +0000

    deploy: 0a9b0f97387749da57ee95f1c476f8555b8cec9d

--- search/typedefs_f.js
+++ search/typedefs_f.js
@@ -4,8 +4,8 @@ var searchData=
   ['sea_1',['Sea',['../classthorin_1_1_world.html#a9cfe62f63052c440c0b6b8867ae7f0b2',1,'thorin::World']]],
   ['set_2',['Set',['../classthorin_1_1_c_f_g.html#a5627c508042392a0bdad6a46e7326804',1,'thorin::CFG']]],
   ['size_5ftype_3',['size_type',['../classthorin_1_1detail_1_1_hash_table.html#a9a099fd5020d99d19eaefcbf32b8f4bd',1,'thorin::detail::HashTable::size_type()'],['../classthorin_1_1_hash_set.html#a600e93e2caab937de4d92eef1687d152',1,'thorin::HashSet::size_type()'],['../classthorin_1_1_hash_map.html#ab57f01fc6536e1567a2ed8cc17e67598',1,'thorin::HashMap::size_type()']]],
-  ['super_4',['super',['../classthorin_1_1filter__iterator.html#afde99daea34bd3f3ffa5796a6a2659b6',1,'thorin::filter_iterator']]],
-  ['super_5',['Super',['../classthorin_1_1_hash_set.html#ace90f040df013b6934beb56ece67b070',1,'thorin::HashSet::Super()'],['../classthorin_1_1_hash_map.html#a3d99bd738376f0ac79912421dc54659c',1,'thorin::HashMap::Super()']]],
+  ['super_4',['Super',['../classthorin_1_1_hash_set.html#ace90f040df013b6934beb56ece67b070',1,'thorin::HashSet::Super()'],['../classthorin_1_1_hash_map.html#a3d99bd738376f0ac79912421dc54659c',1,'thorin::HashMap::Super()']]],
+  ['super_5',['super',['../classthorin_1_1filter__iterator.html#afde99daea34bd3f3ffa5796a6a2659b6',1,'thorin::filter_iterator']]],
   ['symmap_6',['SymMap',['../namespacethorin.html#a74e415ecc6a0cb960a8200c4b293e273',1,'thorin']]],
   ['symset_7',['SymSet',['../namespacethorin.html#a90298178d3972e29d1183793ff142e83',1,'thorin']]]
 ];
--- structthorin_1_1_world_1_1_arena_1_1_lock.js
+++ structthorin_1_1_world_1_1_arena_1_1_lock.js
@@ -1,4 +1,5 @@
 var structthorin_1_1_world_1_1_arena_1_1_lock =
 [
+    [ "Lock", "structthorin_1_1_world_1_1_arena_1_1_lock.html#abd11f75f3d985361bbd4ec77260ba43f", null ],
     [ "~Lock", "structthorin_1_1_world_1_1_arena_1_1_lock.html#afa07a5bf3efdfaa263b3806aae4f93fe", null ]
 ];
