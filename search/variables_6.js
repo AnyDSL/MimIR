57d76dcfacc1f80141630275f8a1051bea225877
leissa <leissa@users.noreply.github.com>
2022-02-03 18:15:17 +0000

    deploy: 272cca846b574130bbb98b85dd7e02c64a917af1

--- search/variables_6.js
+++ search/variables_6.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['gid_5f_0',['gid_',['../classthorin_1_1_def.html#ad787194dde14538ab209e0003318c0cd',1,'thorin::Def']]],
-  ['guard_5f_1',['guard_',['../structthorin_1_1_world_1_1_arena_1_1_lock.html#a21650a9ec3de6423a7c3274d5a7a0693',1,'thorin::World::Arena::Lock']]]
+  ['gid_5f_0',['gid_',['../classthorin_1_1_def.html#ad787194dde14538ab209e0003318c0cd',1,'thorin::Def']]]
 ];
