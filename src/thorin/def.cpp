3d7d811b8c8e230932468e706b24aea69a6b6515
Roland Leißa <leissa@uni-mannheim.de>
2022-02-16 18:53:48 +0100

    clang-format + doc

--- src/thorin/def.cpp
+++ src/thorin/def.cpp
@@ -5,6 +5,7 @@
 
 #include "thorin/rewrite.h"
 #include "thorin/world.h"
+
 #include "thorin/analyses/scope.h"
 
 namespace thorin {
@@ -23,8 +24,7 @@ Def::Def(node_t node, const Def* type, Defs ops, fields_t fields, const Def* dbg
     , order_(0)
     , num_ops_(ops.size())
     , dbg_(dbg)
-    , type_(type)
-{
+    , type_(type) {
     gid_ = world().next_gid();
     std::ranges::copy(ops, ops_ptr());
 
@@ -32,8 +32,7 @@ Def::Def(node_t node, const Def* type, Defs ops, fields_t fields, const Def* dbg
         hash_ = murmur3(gid());
     } else {
         hash_ = type->gid();
-        for (auto op : ops)
-            hash_ = murmur3(hash_, u32(op->gid()));
+        for (auto op : ops) hash_ = murmur3(hash_, u32(op->gid()));
         hash_ = murmur3(hash_, fields_);
         hash_ = murmur3_rest(hash_, u8(node));
         hash_ = murmur3_finalize(hash_, num_ops());
@@ -50,21 +49,18 @@ Def::Def(node_t node, const Def* type, size_t num_ops, fields_t fields, const De
     , order_(0)
     , num_ops_(num_ops)
     , dbg_(dbg)
-    , type_(type)
-{
-    gid_ = world().next_gid();
+    , type_(type) {
+    gid_  = world().next_gid();
     hash_ = murmur3(gid());
     std::fill_n(ops_ptr(), num_ops, nullptr);
     if (!type->no_dep()) type->uses_.emplace(this, -1);
 }
 
 Kind::Kind(World& world)
-    : Def(Node, (const Def*) world.space(), Defs{}, 0, nullptr)
-{}
+    : Def(Node, (const Def*)world.space(), Defs{}, 0, nullptr) {}
 
 Nat::Nat(World& world)
-    : Def(Node, world.kind(), Defs{}, 0, nullptr)
-{}
+    : Def(Node, world.kind(), Defs{}, 0, nullptr) {}
 
 // clang-format off
 
@@ -113,7 +109,10 @@ Arr*   Arr  ::stub(World& w, const Def* t, const Def* dbg) { return w.nom_arr  (
 
 // clang-format on
 
-template<bool up> TBound<up>* TBound<up>::stub(World& w, const Def* t, const Def* dbg) { return w.nom_bound<up>(t, num_ops(), dbg); }
+template<bool up>
+TBound<up>* TBound<up>::stub(World& w, const Def* t, const Def* dbg) {
+    return w.nom_bound<up>(t, num_ops(), dbg);
+}
 
 /*
  * restructure
@@ -137,8 +136,9 @@ const Def* Arr::restructure() {
 
 std::string_view Def::node_name() const {
     switch (node()) {
-#define CODE(op, abbr) case Node::op: return #abbr;
-THORIN_NODE(CODE)
+#define CODE(op, abbr) \
+    case Node::op: return #abbr;
+        THORIN_NODE(CODE)
 #undef CODE
         default: THORIN_UNREACHABLE;
     }
@@ -203,13 +203,10 @@ const Def* Def::arity() const {
 bool Def::equal(const Def* other) const {
     if (isa<Space>() || this->isa_nom() || other->isa_nom()) return this == other;
 
-    bool result =  this->node()    == other->node()
-                && this->fields()  == other->fields()
-                && this->num_ops() == other->num_ops()
-                && this->type()    == other->type();
+    bool result = this->node() == other->node() && this->fields() == other->fields() &&
+                  this->num_ops() == other->num_ops() && this->type() == other->type();
 
-    for (size_t i = 0, e = num_ops(); result && i != e; ++i)
-        result &= this->op(i) == other->op(i);
+    for (size_t i = 0, e = num_ops(); result && i != e; ++i) result &= this->op(i) == other->op(i);
 
     return result;
 }
@@ -224,15 +221,15 @@ const Def* Def::debug_history() const {
 }
 
 void Def::set_name(std::string_view n) const {
-    auto& w = world();
+    auto& w   = world();
     auto name = w.tuple_str(n);
 
     if (dbg_ == nullptr) {
-        auto file = w.tuple_str("");
+        auto file  = w.tuple_str("");
         auto begin = w.lit_nat_max();
         auto finis = w.lit_nat_max();
-        auto meta = w.bot(w.bot_kind());
-        dbg_ = w.tuple({name, w.tuple({file, begin, finis}), meta});
+        auto meta  = w.bot(w.bot_kind());
+        dbg_       = w.tuple({name, w.tuple({file, begin, finis}), meta});
     } else {
         dbg_ = w.insert(dbg_, 3_s, 0_s, name);
     }
@@ -257,19 +254,17 @@ void Def::finalize() {
     }
 
     assert(!dbg() || dbg()->no_dep());
-    if (isa<Pi>())  ++order_;
+    if (isa<Pi>()) ++order_;
     if (auto var = isa<Var>()) {
         var->nom()->var_ = true;
-        dep_ = Dep::Var;
+        dep_             = Dep::Var;
     }
 
     if (isa<Proxy>()) {
         proxy_ = true;
     } else {
-        for (auto op : extended_ops())
-            proxy_ |= op->contains_proxy();
+        for (auto op : extended_ops()) proxy_ |= op->contains_proxy();
     }
-
 }
 
 Def* Def::set(size_t i, const Def* def) {
@@ -278,8 +273,8 @@ Def* Def::set(size_t i, const Def* def) {
 
     if (def != nullptr) {
         assert(i < num_ops() && "index out of bounds");
-        ops_ptr()[i] = def;
-        order_ = std::max(order_, def->order_);
+        ops_ptr()[i]  = def;
+        order_        = std::max(order_, def->order_);
         const auto& p = def->uses_.emplace(this, i);
         assert_unused(p.second);
     }
@@ -301,8 +296,7 @@ bool Def::is_set() const {
         return true;
     }
 
-    if (std::ranges::all_of(ops(), [](auto op) { return op != nullptr; }))
-        return true;
+    if (std::ranges::all_of(ops(), [](auto op) { return op != nullptr; })) return true;
 
     assert(std::ranges::all_of(ops(), [](auto op) { return op == nullptr; }) && "some operands are set, others aren't");
     return false;
@@ -316,12 +310,12 @@ std::string Def::unique_name() const { return name() + "_" + std::to_string(gid(
 
 void Def::replace(Tracker with) const {
     world().DLOG("replace: {} -> {}", this, with);
-    //assert(type() == with->type());
+    // assert(type() == with->type());
     assert(!is_replaced());
 
     if (this != with) {
         for (auto& use : copy_uses()) {
-            auto def = const_cast<Def*>(use.def());
+            auto def   = const_cast<Def*>(use.def());
             auto index = use.index();
             def->set(index, with);
         }
@@ -403,4 +397,4 @@ template TBound<true >* TBound<true >::stub(World&, const Def*, const Def*);
 
 // clang-format on
 
-}
+} // namespace thorin
