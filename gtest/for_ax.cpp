94c250b2988f1cea27ff5852fcb7e23a4f9a113b
Joachim Meyer <jmeyer@cs.uni-saarland.de>
2022-03-15 08:08:17 +0100

    Rename start->begin, stop->end, cont->yield.

--- gtest/for_ax.cpp
+++ gtest/for_ax.cpp
@@ -7,8 +7,8 @@
 #include "thorin/be/ll/ll.h"
 #include "thorin/pass/fp/beta_red.h"
 #include "thorin/pass/fp/copy_prop.h"
-#include "thorin/pass/fp/eta_red.h"
 #include "thorin/pass/fp/eta_exp.h"
+#include "thorin/pass/fp/eta_red.h"
 #include "thorin/pass/pass.h"
 #include "thorin/pass/rw/lower_for.h"
 
@@ -22,10 +22,12 @@ TEST_P(ForAxiomTest, for) {
     auto i32_t  = w.type_int_width(32);
     auto i64_t  = w.type_int_width(64);
     auto argv_t = w.type_ptr(w.type_ptr(i32_t));
-    const auto [cstart, cstop, cstep] = GetParam();
-    auto lit_start = w.lit_int_width(32, cstart);
-    auto lit_stop = w.lit_int_width(32, cstop);
-    auto lit_step = w.lit_int_width(32, cstep);
+
+    const auto [cbegin, cend, cstep] = GetParam();
+
+    auto lit_begin = w.lit_int_width(32, cbegin);
+    auto lit_end   = w.lit_int_width(32, cend);
+    auto lit_step  = w.lit_int_width(32, cstep);
 
     // Cn [mem, i32, ptr(ptr(i32, 0), 0) Cn [mem, i32]]
     auto main_t = w.cn({mem_t, i32_t, argv_t, w.cn({mem_t, i32_t})});
@@ -33,15 +35,15 @@ TEST_P(ForAxiomTest, for) {
 
     {
         auto accumulator_type = w.sigma({i32_t, i64_t});
-        auto cont_type        = w.cn({mem_t, accumulator_type});
-        auto body_type        = w.cn({mem_t, i32_t, accumulator_type, cont_type});
+        auto yield_type       = w.cn({mem_t, accumulator_type});
+        auto body_type        = w.cn({mem_t, i32_t, accumulator_type, yield_type});
 
         auto body = w.nom_lam(body_type, w.dbg("body"));
         {
-            auto [mem, i, acctpl, cont] = body->vars<4>({w.dbg("mem"), w.dbg("i"), w.dbg("acctpl"), w.dbg("cont")});
-            auto add = w.op(Wrap::add, w.lit_nat(0), w.extract(acctpl, 0_s), i);
+            auto [mem, i, acctpl, yield] = body->vars<4>({w.dbg("mem"), w.dbg("i"), w.dbg("acctpl"), w.dbg("yield")});
+            auto add                     = w.op(Wrap::add, w.lit_nat(0), w.extract(acctpl, 0_s), i);
             auto mul = w.op(Wrap::mul, w.lit_nat(0), w.extract(acctpl, 1_s), w.op(Conv::u2u, i64_t, i));
-            body->app(false, cont, {mem, w.tuple({add, mul})});
+            body->app(false, yield, {mem, w.tuple({add, mul})});
         }
 
         auto brk = w.nom_lam(w.cn({mem_t, accumulator_type}), w.dbg("break"));
@@ -51,7 +53,8 @@ TEST_P(ForAxiomTest, for) {
             auto [mem, acctpl] = brk->vars<2>();
             brk->app(false, ret, {mem, w.extract(acctpl, 0_s)});
             main->set_filter(false);
-            main->set_body(w.op_for({i32_t, i64_t}, main_mem, lit_start, lit_stop, lit_step, {w.lit_int(0), w.lit_int(i64_t, 5)}, body, brk));
+            main->set_body(w.op_for({i32_t, i64_t}, main_mem, lit_begin, lit_end, lit_step,
+                                    {w.lit_int(0), w.lit_int(i64_t, 5)}, body, brk));
         }
     }
 
@@ -76,15 +79,14 @@ TEST_P(ForAxiomTest, for) {
     // TODO make sure that proper clang is in path on Windows
 #ifndef _MSC_VER
     unsigned gt = 0;
-    for(int i = cstart; i < cstop; i += cstep) {
-        gt += i;
-    }
+    for (int i = cbegin; i < cend; i += cstep) { gt += i; }
 
     EXPECT_EQ(0, WEXITSTATUS(std::system("clang test.ll -o `pwd`/test")));
     EXPECT_EQ(gt % 255, WEXITSTATUS(std::system("./test")));
 #endif
 }
 
-// test with these start, stop, step combinations:
-INSTANTIATE_TEST_SUITE_P(ForSteps, ForAxiomTest, testing::Combine(testing::Values(0, 2), testing::Values(0, 4, 8), testing::Values(1, 2, 5)));
-
+// test with these begin, end, step combinations:
+INSTANTIATE_TEST_SUITE_P(ForSteps,
+                         ForAxiomTest,
+                         testing::Combine(testing::Values(0, 2), testing::Values(0, 4, 8), testing::Values(1, 2, 5)));
--- src/thorin/pass/rw/lower_for.cpp
+++ src/thorin/pass/rw/lower_for.cpp
@@ -14,32 +14,32 @@ const Def* LowerFor::rewrite(const Def* def) {
 
         auto org_body  = for_ax->arg(for_ax->num_args() - 2);
         auto body_type = org_body->type()->as<Pi>();
-        auto cont_pi   = body_type->doms().back()->as<Pi>();
-        auto cont_lam  = w.nom_lam(cont_pi, w.dbg("continue"));
+        auto yield_pi  = body_type->doms().back()->as<Pi>();
+        auto yield_lam = w.nom_lam(yield_pi, w.dbg("yield"));
 
-        { // construct cont
-            auto [mem, iter, stop, step, acc, body, brk] =
-                for_lam->vars<7>({w.dbg("mem"), w.dbg("start"), w.dbg("stop"), w.dbg("step"), w.dbg("acc"),
+        { // construct yield
+            auto [mem, iter, end, step, acc, body, brk] =
+                for_lam->vars<7>({w.dbg("mem"), w.dbg("begin"), w.dbg("end"), w.dbg("step"), w.dbg("acc"),
                                   w.dbg("body"), w.dbg("break")});
-            auto [cont_mem, cont_acc] = cont_lam->vars<2>();
+            auto [yield_mem, yield_acc] = yield_lam->vars<2>();
 
             auto add = w.op(Wrap::add, w.lit_nat_0(), iter, step);
-            cont_lam->app(false, for_lam, {cont_mem, add, stop, step, cont_acc, body, brk});
+            yield_lam->app(false, for_lam, {yield_mem, add, end, step, yield_acc, body, brk});
         }
         { // construct for
-            auto [mem, iter, stop, step, acc, body, brk] = for_lam->vars<7>();
+            auto [mem, iter, end, step, acc, body, brk] = for_lam->vars<7>();
 
             // continue
             auto if_then_cn = w.cn(w.type_mem());
             auto if_then    = w.nom_lam(if_then_cn, nullptr);
-            if_then->app(false, body, {if_then->mem_var(w.dbg("mem")), iter, acc, cont_lam});
+            if_then->app(false, body, {if_then->mem_var(w.dbg("mem")), iter, acc, yield_lam});
 
             // break
             auto if_else_cn = w.cn(w.type_mem());
             auto if_else    = w.nom_lam(if_else_cn, nullptr);
             if_else->app(false, brk, {if_else->mem_var(w.dbg("mem")), acc});
 
-            auto cmp = w.op(ICmp::ul, iter, stop);
+            auto cmp = w.op(ICmp::ul, iter, end);
             for_lam->branch(false, cmp, if_then, if_else, mem);
         }
 
