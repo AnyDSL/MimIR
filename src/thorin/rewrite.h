5747ac1c68826a7504ada8790fafe31388c305cf
Roland Leißa <leissa@uni-mannheim.de>
2022-03-16 17:34:04 +0100

    doxygen

--- src/thorin/rewrite.h
+++ src/thorin/rewrite.h
@@ -33,16 +33,16 @@ const Def* rewrite(const Def* def, const Def* old_def, const Def* new_def, const
 /// Rewrites @p nom's @p i^th op by substituting @p nom's @p Var with @p arg while obeying @p nom's @p scope.
 const Def* rewrite(Def* nom, const Def* arg, size_t i);
 
-/// Same as above but uses @p scope as an optimization instead of computing a new @p Scope.
+/// Same as above but uses @p scope as an optimization instead of computing a new Scope.
 const Def* rewrite(Def* nom, const Def* arg, size_t i, const Scope& scope);
 
 /// Rewrites @p nom's ops by substituting @p nom's @p Var with @p arg while obeying @p nom's @p scope.
 DefArray rewrite(Def* nom, const Def* arg);
 
-/// Same as above but uses @p scope as an optimization instead of computing a new @p Scope.
+/// Same as above but uses @p scope as an optimization instead of computing a new Scope.
 DefArray rewrite(Def* nom, const Def* arg, const Scope& scope);
 
-/// Removes unreachable and dead code by rebuilding the whole @p world into a new @p World.
+/// Removes unreachable and dead code by rebuilding the whole @p world into a new World.
 void cleanup(World& world);
 
 }
