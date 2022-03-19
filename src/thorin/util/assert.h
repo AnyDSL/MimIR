727ae0d029cd7c135c24096199e6acd6b162a2fc
Roland Leißa <leissa@uni-mannheim.de>
2022-03-19 17:21:03 +0100

    clang-format

--- src/thorin/util/assert.h
+++ src/thorin/util/assert.h
@@ -9,9 +9,15 @@ namespace thorin {
 
 // see https://stackoverflow.com/a/65258501
 #ifdef __GNUC__ // GCC 4.8+, Clang, Intel and other compilers compatible with GCC (-std=c++0x or above)
-[[noreturn]] inline __attribute__((always_inline)) void unreachable([[maybe_unused]] const char* s = {}) { assert(false && s); __builtin_unreachable(); }
+[[noreturn]] inline __attribute__((always_inline)) void unreachable([[maybe_unused]] const char* s = {}) {
+    assert(false && s);
+    __builtin_unreachable();
+}
 #elif defined(_MSC_VER) // MSVC
-[[noreturn]] __forceinline void unreachable([[maybe_unused]] const char* s = {}) { assert(false && s); __assume(false); }
+[[noreturn]] __forceinline void unreachable([[maybe_unused]] const char* s = {}) {
+    assert(false && s);
+    __assume(false);
+}
 #else                   // ???
 inline void unreachable([[maybe_unused]] const char* s) { assert(false && s); }
 #endif
