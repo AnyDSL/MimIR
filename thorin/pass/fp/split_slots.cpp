d1a75099ce15cead8dabfcc3edf9158af4ed1974
Roland Leißa <leissa@uni-mannheim.de>
2022-04-06 03:28:32 +0200

    clang-format

--- thorin/pass/fp/split_slots.cpp
+++ thorin/pass/fp/split_slots.cpp
@@ -7,21 +7,18 @@ namespace thorin {
 
 const Def* SplitSlots::rewrite(const Def* def) {
     if (auto slot = isa<Tag::Slot>(def)) {
-        auto [mem, id] = slot->args<2>();
-        auto [_, ptr]  = slot->projs<2>();
+        auto [mem, id]             = slot->args<2>();
+        auto [_, ptr]              = slot->projs<2>();
         auto [pointee, addr_space] = as<Tag::Ptr>(ptr->type())->args<2>();
 
         if (auto sigma = pointee->isa<Sigma>()) {
-            for (size_t i = 0, e = sigma->num_ops(); i != e; ++i) {
-            }
+            for (size_t i = 0, e = sigma->num_ops(); i != e; ++i) {}
         }
     }
 
     return def;
 }
 
-undo_t SplitSlots::analyze(const Def* def) {
-    return No_Undo;
-}
+undo_t SplitSlots::analyze(const Def* def) { return No_Undo; }
 
 } // namespace thorin
