364edf9cf3df6e5d3478c55e8e6fb00d9acbf9da
Roland Leißa <leissa@uni-mannheim.de>
2022-05-21 00:35:53 +0200

    move some helpers to thorin::sys

---
+++ thorin/util/sys.h
@@ -0,0 +1,24 @@
+#ifndef THORIN_UTIL_SYS_H
+#define THORIN_UTIL_SYS_H
+
+#ifdef _WIN32
+#    define THORIN_WHICH "where"
+#else
+#    define THORIN_WHICH "which"
+#endif
+
+#include <filesystem>
+#include <optional>
+#include <string>
+
+namespace thorin::sys {
+
+/// @returns `std::nullopt` if an error occurred.
+std::optional<std::filesystem::path> path_to_cur_exe();
+
+/// Executes command @p cmd. @returns the output as string.
+std::string exec(const char* cmd);
+
+}
+
+#endif
