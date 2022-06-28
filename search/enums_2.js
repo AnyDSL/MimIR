d50b63bbb61a3f3d5aa1c7e282f64759043c72ae
leissa <leissa@users.noreply.github.com>
2022-06-28 23:17:26 +0000

    deploy: 1ad0933ad1c64bdcc9e60be46c2fd8ac3e171447

--- search/enums_2.js
+++ search/enums_2.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['cc_0',['CC',['../classthorin_1_1Lam.html#aa847d68653879fa7f7e1eb6531174f90',1,'thorin::Lam']]],
   ['clos_1',['clos',['../namespacethorin_1_1clos.html#a4f790c1b00df9b8c0c241b5fb060c293',1,'thorin::clos']]],
-  ['conv_2',['conv',['../namespacethorin_1_1core.html#a016fae77d6f5857b429ceb5b433c4937',1,'thorin::core']]],
-  ['conv_3',['Conv',['../namespacethorin.html#a6216d5351f2eabcae4ab300001103a4f',1,'thorin']]]
+  ['conv_2',['Conv',['../namespacethorin.html#a6216d5351f2eabcae4ab300001103a4f',1,'thorin']]],
+  ['conv_3',['conv',['../namespacethorin_1_1core.html#a016fae77d6f5857b429ceb5b433c4937',1,'thorin::core']]]
 ];
