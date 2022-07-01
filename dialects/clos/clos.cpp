2273222b47be9e86e1ff791fc74f70bce5216949
Roland Leißa <leissa@uni-mannheim.de>
2022-07-01 15:47:03 +0200

    clang-format

--- dialects/clos/clos.cpp
+++ dialects/clos/clos.cpp
@@ -1,14 +1,13 @@
+#include "dialects/clos/clos.h"
+
 #include <thorin/config.h>
 #include <thorin/pass/pass.h>
 
 #include "thorin/dialects.h"
 
-#include "dialects/clos/clos.h"
-
 using namespace thorin;
 
 extern "C" THORIN_EXPORT DialectInfo thorin_get_dialect_info() {
-    return {"clos",
-            [](PipelineBuilder& builder) {},
-            nullptr, [](Normalizers& normalizers) { clos::register_normalizers(normalizers); }};
+    return {"clos", [](PipelineBuilder& builder) {}, nullptr,
+            [](Normalizers& normalizers) { clos::register_normalizers(normalizers); }};
 }
