17e78f5bc56ad24754819c6ce8c0696724f23995
Roland Leißa <leissa@uni-mannheim.de>
2022-08-07 13:12:49 +0200

    clang-format

--- thorin/phase/phase.cpp
+++ thorin/phase/phase.cpp
@@ -19,7 +19,7 @@ const Def* RewritePhase::rewrite(const Def* old_def) {
     if (auto i = old2new_.find(old_def); i != old2new_.end()) return i->second;
 
     if (auto [pre, recurse] = pre_rewrite(old_def); pre) {
-        auto new_def = recurse ? rewrite(pre) : pre;
+        auto new_def         = recurse ? rewrite(pre) : pre;
         return old2new_[pre] = new_def;
     }
 
@@ -31,7 +31,7 @@ const Def* RewritePhase::rewrite(const Def* old_def) {
     }
 
     if (auto old_nom = old_def->isa_nom()) {
-        auto new_nom     = old_nom->stub(new_, new_type, new_dbg);
+        auto new_nom      = old_nom->stub(new_, new_type, new_dbg);
         old2new_[old_nom] = new_nom;
 
         for (size_t i = 0, e = old_nom->num_ops(); i != e; ++i) {
@@ -47,11 +47,11 @@ const Def* RewritePhase::rewrite(const Def* old_def) {
     auto new_def = old_def->rebuild(new_, new_type, new_ops, new_dbg);
 
     if (auto [post, recurse] = pre_rewrite(old_def); post) {
-        auto new_def = recurse ? rewrite(post) : post;
+        auto new_def          = recurse ? rewrite(post) : post;
         return old2new_[post] = new_def;
     }
 
     return old2new_[old_def] = new_def;
 }
 
-}
+} // namespace thorin
