b2c147de0aab59b7cc00f32e3c2a5c4e35026b64
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2022-09-28 16:01:25 +0200

    cleanup

--- dialects/direct/passes/cps2ds.h
+++ dialects/direct/passes/cps2ds.h
@@ -4,7 +4,6 @@
 #include <thorin/pass/pass.h>
 
 namespace thorin::direct {
-// TODO: fix order: entered before other pass rewrites
 
 /// second part of ds2cps
 /// replaces all ds call sites of cps (or ds converted) functions
@@ -23,16 +22,11 @@ public:
 private:
     Def2Def rewritten_lams;
     Def2Def rewritten_;
-    // Def2Def rewritten_bodies_;
     Lam* curr_lam_ = nullptr;
 
     void rewrite_lam(Lam* lam);
     const Def* rewrite_body(const Def*);
     const Def* rewrite_body_(const Def*);
-
-    // void rewrite_lam(Lam* lam);
-    // const Def* rewrite_(const Def*);
-    // const Def* rewrite_inner(const Def*);
 };
 
 } // namespace thorin::direct
