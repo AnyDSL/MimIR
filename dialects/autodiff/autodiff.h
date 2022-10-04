9269e313573fc8796496168d1c12d67853380e56
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2022-10-04 16:11:33 +0200

    cleanup

--- dialects/autodiff/autodiff.h
+++ dialects/autodiff/autodiff.h
@@ -6,21 +6,10 @@
 
 namespace thorin::autodiff {
 
-/*
-
-See the paper [TODO] for a description of the autodiff dialect.
-
-*/
-
-// inline is important
-// otherwise, the compilation creates two definitions of the functions (irregardless of include guards)
 inline const Def* op_autodiff(const Def* fun) {
     World& world = fun->world();
     // we rely on the normalized thorin convention that all arguments in functions are grouped
-    // cn[[args], cont?=cn[returns]]
-    // TODO: do we need mem special casing?
-    // auto [dom, codom] = fun->type()->as<Pi>()->doms<2>();
-    // return world.app(world.app(world.ax<autodiff>(), {dom, codom}), fun);
+    // cn[[args], cont:=cn[returns]]
     return world.app(world.app(world.ax<autodiff>(), fun->type()), fun);
 }
 
