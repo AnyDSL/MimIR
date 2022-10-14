9a535ffccf83a0eba1d09ab56bd12ddf99bd8b56
leissa <leissa@users.noreply.github.com>
2022-10-14 12:16:33 +0000

    deploy: 3524f779ebd99d171e8bc79607ce10eb2cd5acf5

--- autodiff_2normalizers_8cpp.js
+++ autodiff_2normalizers_8cpp.js
@@ -1,9 +1,9 @@
 var autodiff_2normalizers_8cpp =
 [
+    [ "normalize_ad", "autodiff_2normalizers_8cpp.html#a3e5708f556626d708f7c47a07b3fb7ef", null ],
+    [ "normalize_AD", "autodiff_2normalizers_8cpp.html#ad0655fad190a22986ca9c51b4c8e745f", null ],
     [ "normalize_add", "autodiff_2normalizers_8cpp.html#a433fab6b76f44207a6f322e958e49c79", null ],
-    [ "normalize_autodiff", "autodiff_2normalizers_8cpp.html#ac8b68b4b18de4500d22f5a2d5deaf65f", null ],
-    [ "normalize_autodiff_type", "autodiff_2normalizers_8cpp.html#ac4b44d6c80088510721d5e29e06401f4", null ],
     [ "normalize_sum", "autodiff_2normalizers_8cpp.html#a4a6c69b660a28849fe64d4712d60e48d", null ],
-    [ "normalize_tangent_type", "autodiff_2normalizers_8cpp.html#aa0f62e0f2c7d993a4616a5c0de3cd1c8", null ],
+    [ "normalize_Tangent", "autodiff_2normalizers_8cpp.html#ab2a562e173258c7c0d0aba2f8fc7fdf9", null ],
     [ "normalize_zero", "autodiff_2normalizers_8cpp.html#a2ba4143282cde5c7fce7b791b6e3f7e3", null ]
 ];
--- search/enums_d.js
+++ search/enums_d.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['tag_0',['Tag',['../classthorin_1_1fe_1_1Tok.html#aec20e733703a47cbb2a949219dc77b79',1,'thorin::fe::Tok']]],
-  ['tangent_5ftype_1',['tangent_type',['../namespacethorin_1_1autodiff.html#aab54ff4aa3bf48288c868fdde73833df',1,'thorin::autodiff']]],
+  ['tangent_1',['Tangent',['../namespacethorin_1_1autodiff.html#aca9eca4cf9202fc273ba83555948911a',1,'thorin::autodiff']]],
   ['trait_2',['trait',['../namespacethorin_1_1core.html#aa0072b0cc07c0996ceb3516d925c5fbf',1,'thorin::core']]]
 ];
