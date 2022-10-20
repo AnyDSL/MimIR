91cec19221d6bd6ff3f965157fdda79fdff792a9
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2022-10-20 15:25:19 +0200

    completed other operations lowering onto pointer level

--- dialects/matrix/passes/lower_matrix_lowlevel.cpp
+++ dialects/matrix/passes/lower_matrix_lowlevel.cpp
@@ -40,6 +40,30 @@ const Def* op_nop(const Def* a, const Def* b, NOpKind kind) {
     // return c;
 }
 
+const Def* op_lea_tuple(const Def* arr, const Def* tuple) {
+    // mem::op_lea(arr, tuple);
+    auto n       = tuple->num_projs();
+    auto element = arr;
+    for (size_t i = 0; i < n; ++i) { element = mem::op_lea(element, tuple->proj(i)); }
+    return element;
+}
+
+const Def* op_pack_tuple(u64 n, const Def* tuple, const Def* val) {
+    auto& world = val->world();
+    // TODO: find out why num_projs is wrong
+    // auto n = val->num_projs();
+    // world.DLOG("create {} dimensional pack", n);
+    auto element = val;
+    for (int i = n - 1; i >= 0; i--) {
+        auto dim = tuple->proj(i);
+        // world.DLOG("dim {}: {}", i, dim);
+        element = world.pack(dim, element);
+    }
+    world.DLOG("op_pack_tuple: {} -> {}", val, element);
+    world.DLOG("  for tuple: {} : {}", tuple, tuple->type());
+    return element;
+}
+
 // const Def* computeSize(const Def* S) {
 //     auto& world = S->world();
 //     auto n      = S->num_projs();
@@ -123,16 +147,35 @@ const Def* LowerMatrixLowLevel::rewrite_(const Def* def) {
 
         return ptr_ty;
     } else if (auto init_ax = match<matrix::init>(def)) {
-        auto [n, S, T, mem] = init_ax->args<4>();
-        auto arr_ty         = arrTyOfMatrixTy(S, T);
-        auto addr_space     = world.lit_nat_0();
-        auto ptr_mat        = world.app(world.app(world.ax<mem::alloc>(), {arr_ty, addr_space}), mem);
-        return ptr_mat;
+        auto [n, S, T, mem]  = init_ax->args<4>();
+        auto arr_ty          = arrTyOfMatrixTy(S, T);
+        auto [mem2, ptr_mat] = mem::op_alloc(arr_ty, mem)->projs<2>();
+        return world.tuple({mem2, ptr_mat});
     } else if (auto read_ax = match<matrix::read>(def)) {
         auto [mem, mat, idx] = read_ax->args<3>();
-
+        // TODO: check if mat is already converted
+        auto element_ptr = op_lea_tuple(mat, idx);
+        auto [mem2, val] = mem::op_load(mem, element_ptr)->projs<2>();
+        return world.tuple({mem2, val});
     } else if (auto insert_ax = match<matrix::insert>(def)) {
+        auto [mem, mat, idx, val] = insert_ax->args<4>();
+        auto element_ptr          = op_lea_tuple(mat, idx);
+        auto mem2                 = mem::op_store(mem, element_ptr, val);
+        return mem2;
     } else if (auto const_ax = match<matrix::constMat>(def)) {
+        auto [mem, val]      = const_ax->args<2>();
+        auto [n_def, S, T]   = const_ax->callee()->as<App>()->args<3>();
+        auto arr_ty          = arrTyOfMatrixTy(S, T);
+        auto [mem2, ptr_mat] = mem::op_alloc(arr_ty, mem)->projs<2>();
+
+        // store initial value
+        auto n       = n_def->as<Lit>()->get<u64>();
+        auto initial = op_pack_tuple(n, S, val);
+
+        // TODO: test if this is a valid initialization
+        auto mem3 = mem::op_store(mem2, ptr_mat, initial);
+
+        return world.tuple({mem3, ptr_mat});
     }
 
     return def;
