7ac07154934cebc1d71dcb21ed928eb92c000f6b
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2022-10-21 08:32:02 +0200

    added newlines to make it more readable

--- thorin/error.cpp
+++ thorin/error.cpp
@@ -23,7 +23,7 @@ void ErrorHandler::index_out_of_range(const Def* arity, const Def* index, const 
 
 void ErrorHandler::ill_typed_app(const Def* callee, const Def* arg, const Def* dbg) {
     Debug d(dbg ? dbg : arg->dbg());
-    err(d.loc, "cannot pass argument '{}' of type '{}' to '{}' of domain '{}'", arg, arg->type(), callee,
+    err(d.loc, "cannot pass argument \n'{}' of type \n'{}' to \n'{}' of domain \n'{}'", arg, arg->type(), callee,
         callee->type()->as<Pi>()->dom());
 }
 
