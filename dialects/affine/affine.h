dadd32b652c110ea60f664fc86c710c4d5b7a16e
christopherhjung <christopher.h.jung@web.de>
2022-10-25 13:04:41 +0200

    remove legacy mem of affine::op:for

--- dialects/affine/affine.h
+++ dialects/affine/affine.h
@@ -14,15 +14,9 @@ inline const Def* fn_for(World& w, Defs params) {
 
 /// Returns a fully applied affine_for axiom.
 /// See documentation for %affine.For axiom in @ref affine.
-inline const Def* op_for(World& w,
-                         const Def* mem,
-                         const Def* begin,
-                         const Def* end,
-                         const Def* step,
-                         Defs inits,
-                         const Def* body,
-                         const Def* brk) {
+inline const Def*
+op_for(World& w, const Def* begin, const Def* end, const Def* step, Defs inits, const Def* body, const Def* brk) {
     DefArray types(inits.size(), [&](size_t i) { return inits[i]->type(); });
-    return w.app(fn_for(w, types), {mem, begin, end, step, w.tuple(inits), body, brk});
+    return w.app(fn_for(w, types), {begin, end, step, w.tuple(inits), body, brk});
 }
 } // namespace thorin::affine
