d3361302c1ed2031beb8ebde60e5b9e4676582de
Roland Leißa <leissa@uni-mannheim.de>
2022-10-31 01:35:50 +0100

    clang-format

--- dialects/affine/affine.cpp
+++ dialects/affine/affine.cpp
@@ -9,7 +9,7 @@
 extern "C" THORIN_EXPORT thorin::DialectInfo thorin_get_dialect_info() {
     return {"affine",
             [](thorin::PipelineBuilder& builder) {
-        builder.extend_opt_phase([](thorin::PassMan& man) { man.add<thorin::affine::LowerFor>(); });
+                builder.extend_opt_phase([](thorin::PassMan& man) { man.add<thorin::affine::LowerFor>(); });
             },
             nullptr, nullptr};
 }
