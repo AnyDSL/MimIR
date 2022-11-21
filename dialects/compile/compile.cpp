952c4892835b70e0e3bac5e3755f92053bc1e7a2
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2022-11-21 23:44:45 +0100

    handling of passes (per pass phase)

--- dialects/compile/compile.cpp
+++ dialects/compile/compile.cpp
@@ -43,20 +43,38 @@ extern "C" THORIN_EXPORT thorin::DialectInfo thorin_get_dialect_info() {
                     );
                 } ;
 
-            auto pass_phase_flag = flags_t(Axiom::Base<thorin::compile::pass_of_phase>);
+            auto pass_phase_flag = flags_t(Axiom::Base<thorin::compile::pass_phase>);
             passes[pass_phase_flag] = 
-                [](World& world, PipelineBuilder& builder, const Def* app) {
+                [&](World& world, PipelineBuilder& builder, const Def* app) {
                     // auto [ax, pass_phase] = collect_args(app);
-                    auto [ax,passes] = collect_args(app->as<App>()->arg());
+                    auto [ax,pass_list] = collect_args(app->as<App>()->arg());
                     auto last_phase = builder.last_phase();
 
                     // TODO: get passes from axioms (another passes array?)
                     // TODO: maybe everything one level lower (phase -> passman), pass -> ? (something that allows composing it here)
+
+                    // Concept: We create a convention that passes register in the pipeline using append_**in**_last.
+                    // This pass then calls the registered passes in the order they were registered in the last phase.
+
+                    // We create a new dummy phase in which the passes should be inserted.
+                    builder.append_phase_end([](Pipeline& pipe) {});
+
+                    for (auto pass : pass_list) {
+                        auto [pass_def, pass_args] = collect_args(pass);
+                        if (auto pass_ax = pass_def->isa<Axiom>()) {
+                        auto flag = pass_ax->flags();
+                        world.DLOG("pass: {}", pass);
+                        if (passes.contains(flag)) {
+                            auto pass_fun = passes[flag];
+                            pass_fun(world, builder, pass);
+                        }
+                        }
+                    }
                 } ;
 
         }, 
         nullptr,
-        nullptr
-    };
+        // clang-format on
+        };
     // clang-format on
 }
