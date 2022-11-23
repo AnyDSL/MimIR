a95c60921303dfa7d98a0a3fc9f1f763e14e2064
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2022-11-23 09:34:18 +0100

    template for tuple conversion

--- dialects/compile/normalizers.cpp
+++ dialects/compile/normalizers.cpp
@@ -5,6 +5,11 @@ namespace thorin::compile {
 const Def* normalize_pass_phase(const Def* type, const Def* callee, const Def* arg, const Def* dbg) {
     auto& world = type->world();
 
+    // pass_phase (pass_list pass1 ... passn)
+    // -> passes_to_phase n (pass1, ..., passn)
+    // combined_phase (phase_list phase1 ... phasen)
+    // -> phases_to_phase n (phase1, ..., phasen)
+
     return world.raw_app(callee, arg, dbg);
 }
 
