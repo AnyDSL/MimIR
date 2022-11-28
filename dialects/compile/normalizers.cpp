c1312e73f4bf2ef6ca3f11c997b4e9bf0be86530
Roland Leißa <leissa@uni-mannheim.de>
2022-11-28 15:52:21 +0100

    porting to new API

--- dialects/compile/normalizers.cpp
+++ dialects/compile/normalizers.cpp
@@ -9,7 +9,7 @@ const Def* normalize_pass_phase(const Def* type, const Def* callee, const Def* a
 
     auto [ax, _] = collect_args(arg);
     if (ax->flags() != flags_t(Axiom::Base<pass_list>)) {
-        // return world.raw_app(callee, arg, dbg);
+        // return world.raw_app(type, callee, arg, dbg);
         // TODO: remove when normalizers are fixed
         if (ax->flags() == flags_t(Axiom::Base<combine_pass_list>)) {
             auto arg_cpl = arg->as<App>();
@@ -23,7 +23,7 @@ const Def* normalize_pass_phase(const Def* type, const Def* callee, const Def* a
     assert(f_ax->flags() == flags_t(Axiom::Base<pass_list>));
     auto n = pass_list_defs.size();
 
-    return world.raw_app(world.raw_app(world.ax<passes_to_phase>(), world.lit_nat(n)), world.tuple(pass_list_defs));
+    return world.raw_app(type, world.raw_app(type, world.ax<passes_to_phase>(), world.lit_nat(n)), world.tuple(pass_list_defs));
 }
 
 /// `combined_phase (phase_list phase1 ... phasen)` -> `phases_to_phase n (phase1, ..., phasen)`
@@ -34,13 +34,13 @@ const Def* normalize_combined_phase(const Def* type, const Def* callee, const De
     assert(ax->flags() == flags_t(Axiom::Base<phase_list>));
     auto n = phase_list_defs.size();
 
-    return world.raw_app(world.raw_app(world.ax<phases_to_phase>(), world.lit_nat(n)), world.tuple(phase_list_defs));
+    return world.raw_app(type, world.raw_app(type, world.ax<phases_to_phase>(), world.lit_nat(n)), world.tuple(phase_list_defs));
 }
 
 /// `single_pass_phase pass` -> `passes_to_phase 1 pass`
 const Def* normalize_single_pass_phase(const Def* type, const Def* callee, const Def* arg, const Def* dbg) {
     auto& world = type->world();
-    return world.raw_app(world.raw_app(world.ax<passes_to_phase>(), world.lit_nat_1()), arg);
+    return world.raw_app(type, world.raw_app(type, world.ax<passes_to_phase>(), world.lit_nat_1()), arg);
 }
 
 /// `combine_pass_list K (pass_list pass11 ... pass1N) ... (pass_list passK1 ... passKM) = pass_list pass11 ... p1N ...
@@ -55,7 +55,7 @@ const Def* normalize_combine_pass_list(const Def* type, const Def* callee, const
         assert(ax->flags() == flags_t(Axiom::Base<pass_list>));
         passes.insert(passes.end(), pass_list_defs.begin(), pass_list_defs.end());
     }
-    return world.raw_app(world.ax<pass_list>(), world.tuple(passes));
+    return world.raw_app(type, world.ax<pass_list>(), world.tuple(passes));
 }
 
 THORIN_compile_NORMALIZER_IMPL
