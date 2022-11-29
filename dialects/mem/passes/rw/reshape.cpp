a0762787e078b54961172eb2c2bf02403d98e4f4
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2022-11-29 14:41:29 +0100

    fixed handling of main function

--- dialects/mem/passes/rw/reshape.cpp
+++ dialects/mem/passes/rw/reshape.cpp
@@ -95,8 +95,16 @@ Lam* Reshape::reshape_lam(Lam* def) {
     auto pi_ty  = def->type();
     auto new_ty = reshape_type(pi_ty)->as<Pi>();
 
-    Lam* new_lam  = w.nom_lam(new_ty, w.dbg(def->name() + "_reshaped"));
-    old2new_[def] = new_lam;
+    Lam* new_lam;
+    auto name = def->name();
+
+    if (name != "main") {
+        name          = name + "_reshape";
+        new_lam       = w.nom_lam(new_ty, w.dbg(name));
+        old2new_[def] = new_lam;
+    } else {
+        new_lam = def;
+    }
 
     w.DLOG("Reshape lam: {} : {}", def, pi_ty);
     w.DLOG("         to: {} : {}", new_lam, new_ty);
@@ -123,8 +131,8 @@ Lam* Reshape::reshape_lam(Lam* def) {
     new_lam->set_filter(true);
 
     if (def->is_external()) {
-        new_lam->make_external();
         def->make_internal();
+        new_lam->make_external();
     }
 
     w.DLOG("finished transforming: {} : {}", new_lam, new_ty);
