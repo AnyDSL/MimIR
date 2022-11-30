e1e58effb050f6012427fa48252bd7f5c0254d3f
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2022-11-30 10:46:31 +0100

    allow for function calls using static beta reduction

--- dialects/compile/compile.h
+++ dialects/compile/compile.h
@@ -8,6 +8,13 @@
 
 namespace thorin::compile {
 inline void handle_optimization_part(const Def* part, World& world, Passes& passes, PipelineBuilder& builder) {
+    if (auto app = part->isa<App>()) {
+        if (auto lam = app->callee()->isa<Lam>()) {
+            part = lam->reduce(app->arg())[1];
+            world.DLOG("reduce pass/phase lambda {} to {} : {}", lam, part, part->type());
+        }
+    }
+
     auto [phase_def, phase_args] = collect_args(part);
     world.DLOG("pass/phase: {}", phase_def);
     if (auto phase_ax = phase_def->isa<Axiom>()) {
@@ -19,8 +26,11 @@ inline void handle_optimization_part(const Def* part, World& world, Passes& pass
             world.WLOG("pass/phase '{}' not found", phase_ax->name());
             assert(passes.contains(flag) && "pass/phase not found");
         }
+    } else if (auto lam = phase_def->isa<Lam>()) {
+        assert(0 && "curried lambas are not supported");
     } else {
         world.WLOG("pass/phase '{}' is not an axiom", phase_def);
+        assert(phase_def->isa<Axiom>() && "pass/phase is not an axiom");
     }
 }
 } // namespace thorin::compile
