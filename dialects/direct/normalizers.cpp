cf90b96b73d298b82efed3ae599012b1afadb89b
Roland Leißa <leissa@uni-mannheim.de>
2022-11-30 02:10:44 +0100

    clang-format

--- dialects/direct/normalizers.cpp
+++ dialects/direct/normalizers.cpp
@@ -5,9 +5,7 @@
 namespace thorin::direct {
 
 /// `cps2ds` is directly converted to `op_cps2ds_dep f` in its normalizer.
-const Def* normalize_cps2ds(const Def*, const Def*, const Def* fun, const Def* dbg) {
-    return op_cps2ds_dep(fun, dbg);
-}
+const Def* normalize_cps2ds(const Def*, const Def*, const Def* fun, const Def* dbg) { return op_cps2ds_dep(fun, dbg); }
 
 THORIN_direct_NORMALIZER_IMPL
 
