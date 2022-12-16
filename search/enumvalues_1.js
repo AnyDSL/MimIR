df4d1738c67b60facb11440b9e7da311d44c5f8c
leissa <leissa@users.noreply.github.com>
2022-12-16 22:38:30 +0000

    deploy: aa28d02f61abefa4c38598a276cca0e2612ea302

--- search/enumvalues_1.js
+++ search/enumvalues_1.js
@@ -1,10 +1,10 @@
 var searchData=
 [
   ['b_0',['b',['../namespacethorin_1_1core.html#a70895f76bd16fdb3db2c75dd1fd38978acae4977ec0f9b75b8b665538afffd5b5',1,'thorin::core::b()'],['../namespacethorin_1_1math.html#a00e74d198c086fe1fb880c87ea3bb8fea142b727ea49fce05521e55d5137130a9',1,'thorin::math::b()']]],
-  ['bl_1',['bl',['../namespacethorin_1_1math.html#a00e74d198c086fe1fb880c87ea3bb8fead9f2588c436d436d3f18a5dadac37efb',1,'thorin::math']]],
-  ['bl_2',['BL',['../namespacethorin_1_1math.html#a00e74d198c086fe1fb880c87ea3bb8fea38015fe0a0f89084d5c4301ba5dfcc85',1,'thorin::math']]],
-  ['bl_3',['bL',['../namespacethorin_1_1math.html#a00e74d198c086fe1fb880c87ea3bb8fea5e258b8b92a1188b1fcf39d2467cc91d',1,'thorin::math']]],
-  ['bl_4',['Bl',['../namespacethorin_1_1math.html#a00e74d198c086fe1fb880c87ea3bb8fea4237abb58fd06eb53c5c9f5f8289ade1',1,'thorin::math']]],
+  ['bl_1',['bL',['../namespacethorin_1_1math.html#a00e74d198c086fe1fb880c87ea3bb8fea5e258b8b92a1188b1fcf39d2467cc91d',1,'thorin::math']]],
+  ['bl_2',['Bl',['../namespacethorin_1_1math.html#a00e74d198c086fe1fb880c87ea3bb8fea4237abb58fd06eb53c5c9f5f8289ade1',1,'thorin::math']]],
+  ['bl_3',['BL',['../namespacethorin_1_1math.html#a00e74d198c086fe1fb880c87ea3bb8fea38015fe0a0f89084d5c4301ba5dfcc85',1,'thorin::math']]],
+  ['bl_4',['bl',['../namespacethorin_1_1math.html#a00e74d198c086fe1fb880c87ea3bb8fead9f2588c436d436d3f18a5dadac37efb',1,'thorin::math']]],
   ['bot_5',['Bot',['../classthorin_1_1EtaRed.html#a4ac1f54e62bd9e356510aa0b4520553fa9cb1d0d2710079355b87aa158e3a54a7',1,'thorin::EtaRed']]],
   ['bot_6',['bot',['../namespacethorin_1_1math.html#aff01396947cc8f294f10ddaec9e8d4caaed12a2391d1a04e3ff2fa64048db4863',1,'thorin::math::bot()'],['../namespacethorin_1_1clos.html#a5c55a5bd62132e28b49915902ea0117aa62cbfe7c9edd7c5f14f4a83cd32a3bcd',1,'thorin::clos::bot()']]],
   ['bot_7',['Bot',['../namespacethorin_1_1Node.html#a8f9ce7cae6b718bac0ca504d3fe29afba68c4ea4a1177ed112e3b590ef39b6538',1,'thorin::Node']]]
--- search/enumvalues_c.js
+++ search/enumvalues_c.js
@@ -6,5 +6,5 @@ var searchData=
   ['m_5fid_3',['M_id',['../classthorin_1_1fe_1_1Tok.html#aec20e733703a47cbb2a949219dc77b79ad43558618211347eb1e180a76a011d7a',1,'thorin::fe::Tok']]],
   ['max_4',['Max',['../namespacethorin_1_1Node.html#a8f9ce7cae6b718bac0ca504d3fe29afba96995d2f451e6ea4dca3296b37f8b3f2',1,'thorin::Node']]],
   ['meet_5',['Meet',['../namespacethorin_1_1Node.html#a8f9ce7cae6b718bac0ca504d3fe29afba1176c238713240939d0ce7767f2c83fd',1,'thorin::Node']]],
-  ['mul_6',['mul',['../namespacethorin_1_1core.html#ab1f42e21a4de8bd328e18b8006b9f74fa91608a3e04d4c29b574a36a550594d56',1,'thorin::core::mul()'],['../namespacethorin_1_1math.html#aa313bc84ec271c77a5d5d1f6b3897ec4aa47625b6c04dd8ae4c3a60d759a0a4e7',1,'thorin::math::mul()']]]
+  ['mul_6',['mul',['../namespacethorin_1_1core.html#a7367a6650ab2feeba43426b8e1853bf7a91608a3e04d4c29b574a36a550594d56',1,'thorin::core::mul()'],['../namespacethorin_1_1math.html#aa313bc84ec271c77a5d5d1f6b3897ec4aa47625b6c04dd8ae4c3a60d759a0a4e7',1,'thorin::math::mul()']]]
 ];
