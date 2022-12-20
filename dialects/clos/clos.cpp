2810df71b2b29ed4286514c419fe070aaade82a1
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2022-12-20 12:09:30 +0100

    resolved merge artifact

--- dialects/clos/clos.cpp
+++ dialects/clos/clos.cpp
@@ -141,11 +141,11 @@ extern "C" THORIN_EXPORT DialectInfo thorin_get_dialect_info() {
     return {"clos",
             [](Passes& passes) {
                 register_pass<clos::clos_conv_prep_pass, clos::ClosConvPrep>(passes, nullptr);
-                register_pass<clos::clos_conv_pass, ClosConvWrapper>(passes);
+                register_pass<clos::clos_conv_pass, clos::ClosConvWrapper>(passes);
                 register_pass<clos::branch_clos_pass, clos::BranchClosElim>(passes);
                 register_pass<clos::lower_typed_clos_prep_pass, clos::LowerTypedClosPrep>(passes);
                 register_pass<clos::clos2sjlj_pass, clos::Clos2SJLJ>(passes);
-                register_pass<clos::lower_typed_clos_pass, LowerTypedClosWrapper>(passes);
+                register_pass<clos::lower_typed_clos_pass, clos::LowerTypedClosWrapper>(passes);
                 // TODO:; remove after ho_codegen merge
                 passes[flags_t(Axiom::Base<clos::eta_red_bool_pass>)] = [&](World&, PipelineBuilder& builder,
                                                                             const Def* app) {
