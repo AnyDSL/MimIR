962aa010532e6b8e89ba8247d6889facdae3c24f
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2023-02-01 17:45:28 +0100

    Revert "attempt at platform independent timing"
    This reverts commit cb34c312b8c650a72d50ce70e7b83fee5855fa8e.

--- lit/clos/lib.c
+++ lit/clos/lib.c
@@ -1,10 +1,8 @@
-#include <bits/types/time_t.h>
 #include <inttypes.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/select.h>
-#include <time.h>
+#include <sys/time.h>
 // #include <setjmp.h>
 
 void print_i32(int32_t i) { printf("%" PRId32 "\n", i); }
@@ -23,28 +21,16 @@ void printNL() {}
 //     return _JBLEN; // for clos::sjlj
 // }
 
-void* get_time() {
-    clock_t* tv = (clock_t*)malloc(sizeof(*tv));
-    *tv = clock();
+void* time() {
+    struct timeval* tv = (struct timeval*)malloc(sizeof(*tv));
+    gettimeofday(tv, NULL);
     return (void*)tv;
 }
 
-static float tdiff(clock_t* start, clock_t* end) {
-    return (float)(*end - *start) / CLOCKS_PER_SEC * 1000;
+static float tdiff(struct timeval* start, struct timeval* end) {
+    return (end->tv_sec - start->tv_sec) + 1e-6 * (end->tv_usec - start->tv_usec);
 }
 
 void print_time_diff(void* tv1, void* tv2) {
-    printf("sys\t%0.3fms \n", tdiff((clock_t*)tv1, (clock_t*)tv2));
+    printf("real\t%0.6f \n", tdiff((struct timeval*)tv1, (struct timeval*)tv2));
 }
-
-// int main() {
-//     // test print_time_diff
-//     void* tv1 = get_time();
-//     // a long computation
-//     for (int i = 0; i < 300000; i++) {
-//         int j = i * i;
-//         printf("%d\n", j);
-//     }
-//     void* tv2 = get_time();
-//     print_time_diff(tv1, tv2);
-// }
