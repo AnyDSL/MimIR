e9d4ace360aa400591e7bbfedbfd6ca43cf16507
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2023-02-02 09:20:35 +0100

    disable timing for non-linux platform

--- lit/clos/lib.c
+++ lit/clos/lib.c
@@ -2,25 +2,38 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#ifdef linux
+// TODO: use platform independent time functions
 #include <sys/time.h>
+#endif
 // #include <setjmp.h>
 
-void print_i32(int32_t i) { printf("%" PRId32 "\n", i); }
+#define SILENT 1
 
-void println_i32(int32_t i) { printf("%" PRId32 "\n", i); }
-void newline() { printf("\n"); }
+#ifdef SILENT
+// less intrusive than redefining printf
+void print_i32(int32_t i) { }
+void println_i32(int32_t i) { }
+void newline() { }
 
-// void printInteger(int i) { printf("%d, ", i); }
-// void printIntegerNL(int i) { printf("%d\n", i); }
-// void printNL() { printf("\n"); }
 void printInteger(int i) {}
 void printIntegerNL(int i) {}
 void printNL() {}
+#else
+void print_i32(int32_t i) { printf("%" PRId32 "\n", i); }
+void println_i32(int32_t i) { printf("%" PRId32 "\n", i); }
+void newline() { printf("\n"); }
+
+void printInteger(int i) { printf("%d, ", i); }
+void printIntegerNL(int i) { printf("%d\n", i); }
+void printNL() { printf("\n"); }
+#endif
 
 // long jmpbuf_size(){
 //     return _JBLEN; // for clos::sjlj
 // }
 
+#ifdef linux
 void* time() {
     struct timeval* tv = (struct timeval*)malloc(sizeof(*tv));
     gettimeofday(tv, NULL);
@@ -34,3 +47,8 @@ static float tdiff(struct timeval* start, struct timeval* end) {
 void print_time_diff(void* tv1, void* tv2) {
     printf("real\t%0.6f \n", tdiff((struct timeval*)tv1, (struct timeval*)tv2));
 }
+#else
+void* time() { return NULL; }
+void print_time_diff(void* tv1, void* tv2) {}
+static float tdiff(struct timeval* start, struct timeval* end) { return 0; }
+#endif
