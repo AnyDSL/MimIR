5600ac4e50168d185bd4fd16b45b9875f1f12c5f
Roland Leißa <leissa@uni-mannheim.de>
2023-03-20 02:41:46 +0100

    clang-format

--- thorin/util/print.h
+++ thorin/util/print.h
@@ -25,15 +25,13 @@ struct Elem {
 namespace detail {
 
 template<class T>
-concept Printable = requires(std::ostream& os, T a) {
-    os << a;
-};
+concept Printable = requires(std::ostream& os, T a) { os << a; };
 
 template<class T>
 concept Elemable = requires(T elem) {
-    elem.range;
-    elem.f;
-};
+                       elem.range;
+                       elem.f;
+                   };
 
 template<class R, class F>
 std::ostream& range(std::ostream& os, const R& r, F f, const char* sep = ", ") {
@@ -41,11 +39,10 @@ std::ostream& range(std::ostream& os, const R& r, F f, const char* sep = ", ") {
     for (const auto& elem : r) {
         for (auto i = cur_sep; *i != '\0'; ++i) os << *i;
 
-        if constexpr (std::is_invocable_v<F, std::ostream&, decltype(elem)>) {
+        if constexpr (std::is_invocable_v<F, std::ostream&, decltype(elem)>)
             std::invoke(f, os, elem);
-        } else {
+        else
             std::invoke(f, elem);
-        }
         cur_sep = sep;
     }
     return os;
@@ -116,7 +113,8 @@ std::ostream& print(std::ostream& os, const char* s, T&& t, Args&&... args) {
                     detail::range(
                         os, t, [&](const auto& x) { os << x; }, spec.c_str());
                 } else {
-                    []<bool flag = false>() { static_assert(flag, "cannot print T t"); }();
+                    []<bool flag = false>() { static_assert(flag, "cannot print T t"); }
+                    ();
                 }
 
                 ++s; // skip closing brace '}'
