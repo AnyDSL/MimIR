6dcc5abee238ff9f7206a1d89774e2e961f19d5a
Roland Leißa <leissa@uni-mannheim.de>
2023-05-08 13:06:24 +0200

    cleanup

--- dialects/matrix/passes/lower_matrix_highlevel.cpp
+++ dialects/matrix/passes/lower_matrix_highlevel.cpp
@@ -12,12 +12,7 @@
 
 namespace thorin::matrix {
 
-Ref LowerMatrixHighLevelMapRed::rewrite(Ref def) {
-    if (auto i = rewritten.find(def); i != rewritten.end()) return i->second;
-    auto new_def   = rewrite_(def);
-    rewritten[def] = new_def;
-    return rewritten[def];
-}
+namespace {
 
 std::optional<Ref> internal_function_of_axiom(const Axiom* axiom, Ref meta_args, Ref args) {
     auto& world      = axiom->world();
@@ -35,22 +30,25 @@ std::optional<Ref> internal_function_of_axiom(const Axiom* axiom, Ref meta_args,
     return std::nullopt;
 }
 
+}
+
+Ref LowerMatrixHighLevelMapRed::rewrite(Ref def) {
+    if (auto i = rewritten.find(def); i != rewritten.end()) return i->second;
+    auto new_def   = rewrite_(def);
+    rewritten[def] = new_def;
+    return rewritten[def];
+}
+
 Ref LowerMatrixHighLevelMapRed::rewrite_(Ref def) {
-    auto& world = def->world();
-
     if (auto mat_ax = match<matrix::prod>(def)) {
-        auto args      = mat_ax->arg();
-        auto meta_args = mat_ax->callee()->as<App>()->arg();
+        auto [mem, M, N]  = mat_ax->args<3>();
+        auto [m, k, l, w] = mat_ax->decurry()->args<4>();
+        auto w_lit        = Lit::isa(w);
 
-        auto [m, k, l, w] = meta_args->projs<4>();
-        auto [mem, M, N]  = args->projs<3>();
-
-        auto w_lit = w->isa<Lit>();
-
-        auto ext_fun = world.external(world.sym("extern_matrix_prod"));
-        if (ext_fun && (w_lit && w_lit->get<u64>() == 64)) {
+        auto ext_fun = world().external(world().sym("extern_matrix_prod"));
+        if (ext_fun && (w_lit && *w_lit == 64)) {
             auto ds_fun  = direct::op_cps2ds_dep(ext_fun);
-            auto fun_app = world.app(ds_fun, {mem, m, k, l, M, N});
+            auto fun_app = world().app(ds_fun, {mem, m, k, l, M, N});
             return fun_app;
         }
     }
@@ -59,8 +57,8 @@ Ref LowerMatrixHighLevelMapRed::rewrite_(Ref def) {
         if (auto inner_app = outer_app->callee()->isa<App>()) {
             if (auto axiom = inner_app->callee()->isa<Axiom>()) {
                 if (auto internal_function = internal_function_of_axiom(axiom, inner_app->arg(), outer_app->arg())) {
-                    world.DLOG("lower matrix axiom {} in {} : {}", *axiom->sym(), def, def->type());
-                    world.DLOG("lower matrix axiom using: {} : {}", *internal_function, (*internal_function)->type());
+                    world().DLOG("lower matrix axiom {} in {} : {}", *axiom->sym(), def, def->type());
+                    world().DLOG("lower matrix axiom using: {} : {}", *internal_function, (*internal_function)->type());
                     return *internal_function;
                 }
             }
