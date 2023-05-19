1ce21b6f2b7297070fe3da8933d048585733a040
Joachim Meyer <jmeyer@cs.uni-saarland.de>
2023-05-19 12:28:20 +0200

    Cleanup `cls_w_impl`.

--- dialects/regex/passes/lower_regex.cpp
+++ dialects/regex/passes/lower_regex.cpp
@@ -15,47 +15,6 @@ Ref wrap_in_cps2ds(Ref callee) {
     return direct::op_cps2ds_dep(callee);
 }
 
-Ref cls_w_impl(Ref re) {
-    auto& world = re->world();
-    return world.annex<regex::match_w>();
-
-    auto match_w = world.mut_lam(re->as<Pi>());
-    match_w->set("__match_w");
-    auto [mem, ptr, idx] = match_w->vars<3>();
-    auto [mem2, chr]     = world.iapp(world.annex<mem::load>(), {mem, mem::op_lea(ptr, idx)})->projs<2>();
-
-    auto i8uge = world.app(world.annex(core::icmp::uge), world.lit_nat(256));
-    auto i8ule = world.app(world.annex(core::icmp::ule), world.lit_nat(256));
-    auto i8e   = world.app(world.annex(core::icmp::e), world.lit_nat(256));
-    auto and0  = world.iapp(world.annex(core::bit2::and_), world.lit_nat_0());
-    auto or0   = world.iapp(world.annex(core::bit2::or_), world.lit_nat_0());
-
-    auto geA = world.app(i8uge, {chr, world.lit_int(8, 'A')});
-    auto gea = world.app(i8uge, {chr, world.lit_int(8, 'a')});
-    auto ge0 = world.app(i8uge, {chr, world.lit_int(8, '0')});
-    auto leZ = world.app(i8ule, {chr, world.lit_int(8, 'Z')});
-    auto lez = world.app(i8ule, {chr, world.lit_int(8, 'z')});
-    auto le9 = world.app(i8uge, {chr, world.lit_int(8, '9')});
-    auto e_  = world.app(i8e, {chr, world.lit_int(8, '_')});
-
-    auto is_upper_lit = world.app(and0, {geA, leZ});
-    auto is_lower_lit = world.app(and0, {gea, lez});
-    auto is_digit_lit = world.app(and0, {ge0, le9});
-
-    auto is_w = world.app(or0, {world.app(or0, {is_upper_lit, is_lower_lit}), world.app(or0, {is_digit_lit, e_})});
-
-    auto updated_pos
-        = world.app(world.app(world.app(world.annex(core::wrap::add), world.lit_nat(4294967296)), world.lit_nat_0()),
-                    {idx, world.lit_int(32, 1)});
-    idx->type()->dump();
-    idx->type()->op(0)->dump();
-    auto new_pos = world.select(
-        world.app(world.iapp(world.annex(core::conv::u), idx->type()->as<App>()->arg(0)), updated_pos), idx, is_w);
-    match_w->set_filter(false);
-    match_w->set_body(world.tuple({mem2, is_w, new_pos}));
-    return match_w;
-}
-
 Ref cls_impl(const Axiom* cls_ax) {
     auto& world = cls_ax->world();
     switch (static_cast<cls>(cls_ax->flags())) {
