d5e4ff7f635699f30cf10b1faf57757ae50ea34c
Joachim Meyer <jmeyer@cs.uni-saarland.de>
2023-05-30 10:36:49 +0200

    Use aggressive lamspec for regex matches.
    Fix normalizers with ranges.

--- dialects/regex/normalizers_regex.cpp
+++ dialects/regex/normalizers_regex.cpp
@@ -1,9 +1,8 @@
 #include <algorithm>
 #include <iterator>
 #include <numeric>
+#include <ranges>
 #include <vector>
-
-#include <bits/ranges_util.h>
 
 #include "thorin/axiom.h"
 #include "thorin/tuple.h"
@@ -46,15 +45,20 @@ Ref normalize_quant(Ref type, Ref callee, Ref arg) {
 }
 
 template<class ConjOrDisj>
-std::vector<const Def*> flatten_in_arg(Ref arg) {
-    std::vector<const Def*> newArgs;
+void flatten_in_arg(Ref arg, std::vector<const Def*>& newArgs) {
     for (const auto* proj : arg->projs()) {
         // flatten conjs in conjs / disj in disjs
         if (auto seq_app = thorin::match<ConjOrDisj>(proj))
-            for (auto seq_arg : seq_app->args()) newArgs.push_back(seq_arg);
+            flatten_in_arg<ConjOrDisj>(seq_app->arg(), newArgs);
         else
             newArgs.push_back(proj);
     }
+}
+
+template<class ConjOrDisj>
+std::vector<const Def*> flatten_in_arg(Ref arg) {
+    std::vector<const Def*> newArgs;
+    flatten_in_arg<ConjOrDisj>(arg, newArgs);
     return newArgs;
 }
 
@@ -78,28 +82,12 @@ Ref normalize_conj(Ref type, Ref callee, Ref arg) {
     return arg;
 }
 
-template<cls A, cls B>
-bool equals_any(const Def* cls0, const Def* cls1) {
-    return (thorin::match(A, cls0) && thorin::match(B, cls1)) || (thorin::match(A, cls1) && thorin::match(B, cls0));
-}
-
-bool equals_any(const Def* lhs, const Def* rhs) {
-    auto check_arg_equiv = [](const Def* lhs, const Def* rhs) {
-        if (auto rng_lhs = thorin::match<range>(lhs))
-            if (auto not_rhs = thorin::match<not_>(rhs))
-                if (auto rng_rhs = thorin::match<range>(not_rhs->arg())) return rng_lhs == rng_rhs;
-        return false;
-    };
-
-    return check_arg_equiv(lhs, rhs) || check_arg_equiv(rhs, lhs);
-}
-
 bool compare_re(const Def* lhs, const Def* rhs) {
     auto lhs_range = thorin::match<range>(lhs);
     auto rhs_range = thorin::match<range>(rhs);
     if (lhs_range) {
         // sort ranges by increasing lower bound
-        if (rhs_range) return Lit::as(rhs_range->arg()->proj(0)) < Lit::as(rhs_range->arg()->proj(0));
+        if (rhs_range) return Lit::as(lhs_range->arg()->proj(0)) < Lit::as(rhs_range->arg()->proj(0));
         // ranges just before the lits
         return false;
     } else if (rhs_range) {
@@ -123,7 +111,6 @@ std::optional<std::pair<nat_t, nat_t>> merge_ranges(std::pair<nat_t, nat_t> a, s
             {std::min(a.first, b.first), std::max(a.second, b.second)}
         };
     }
-    return {};
     return {};
 }
 
@@ -156,6 +143,7 @@ void merge_ranges(std::vector<const Def*>& args) {
 
     for (auto it = oldRanges.begin(); it != oldRanges.end(); ++it) {
         auto current_range = *it;
+        world.DLOG("old range: {}-{}", current_range.first, current_range.second);
         for (auto inner = it + 1; inner != oldRanges.end(); ++inner)
             if (auto merged = merge_ranges(current_range, *inner)) current_range = *merged;
 
@@ -166,7 +154,11 @@ void merge_ranges(std::vector<const Def*>& args) {
                 deDuplicate.push_back(inner);
             }
         }
-        for (auto dedup : deDuplicate) newRanges.erase(dedup);
+        for (auto dedup : deDuplicate) {
+            world.DLOG("dedup {}-{}", current_range.first, current_range.second);
+            newRanges.erase(dedup);
+        }
+        world.DLOG("new range: {}-{}", current_range.first, current_range.second);
         newRanges.push_back(std::move(current_range));
     }
     // invalidates rangesBegin
@@ -174,6 +166,31 @@ void merge_ranges(std::vector<const Def*>& args) {
     std::transform(newRanges.begin(), newRanges.end(), std::back_inserter(args), app_range{world});
 
     make_vector_unique(args);
+}
+
+template<cls A, cls B>
+bool equals_any(const Def* cls0, const Def* cls1) {
+    return (thorin::match(A, cls0) && thorin::match(B, cls1)) || (thorin::match(A, cls1) && thorin::match(B, cls0));
+}
+
+bool equals_any(const Def* lhs, const Def* rhs) {
+    auto check_arg_equiv = [](const Def* lhs, const Def* rhs) {
+        if (auto rng_lhs = thorin::match<range>(lhs))
+            if (auto not_rhs = thorin::match<not_>(rhs)) {
+                if (auto rng_rhs = thorin::match<range>(not_rhs->arg())) return rng_lhs == rng_rhs;
+            }
+        return false;
+    };
+
+    return check_arg_equiv(lhs, rhs) || check_arg_equiv(rhs, lhs);
+}
+
+bool equals_any(const std::vector<const Def*>& lhs, const std::vector<const Def*>& rhs) {
+    std::vector<std::pair<nat_t, nat_t>> lhs_ranges, rhs_ranges;
+    auto only_ranges = std::ranges::views::filter([](auto d) { return match<range>(d); });
+    std::ranges::transform(lhs | only_ranges, std::back_inserter(lhs_ranges), get_range);
+    std::ranges::transform(rhs | only_ranges, std::back_inserter(rhs_ranges), get_range);
+    return std::ranges::includes(lhs_ranges, rhs_ranges) || std::ranges::includes(rhs_ranges, lhs_ranges);
 }
 
 Ref normalize_disj(Ref type, Ref, Ref arg) {
@@ -191,15 +208,28 @@ Ref normalize_disj(Ref type, Ref, Ref arg) {
 
         const Def* toRemove = nullptr;
         for (const auto* cls0 : newArgs)
-            for (const auto* cls1 : newArgs)
+            for (const auto* cls1 : newArgs) {
                 if (equals_any(cls0, cls1)) return world.annex<any>();
+                if (auto not_rhs = thorin::match<not_>(cls1)) {
+                    if (auto disj_rhs = thorin::match<disj>(not_rhs->arg())) {
+                        auto rngs = flatten_in_arg<disj>(disj_rhs->arg());
+                        make_vector_unique(rngs);
+                        if (equals_any(newArgs, rngs)) return world.annex<any>();
+                    }
+                }
+            }
 
         std::erase(newArgs, toRemove);
 
-        if (newArgs.size() > 2) return make_binary_tree<disj>(type, newArgs);
-        if (newArgs.size() > 1)
-            return world.raw_app(type, world.app(world.annex<disj>(), world.lit_nat(2)), world.tuple(newArgs));
-        return newArgs.back();
+        for (const auto* sth : newArgs) world.DLOG("final ranges {}", sth);
+
+        const Def* retval = newArgs.back();
+        if (newArgs.size() > 2)
+            retval = make_binary_tree<disj>(type, newArgs);
+        else if (newArgs.size() > 1)
+            retval = world.raw_app(type, world.app(world.annex<disj>(), world.lit_nat(2)), world.tuple(newArgs));
+        world.DLOG("disj app: {}", retval);
+        return retval;
     }
     return arg;
 }
