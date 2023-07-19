d9953624db460788e5e349257c41977e0005bcfd
Roland Leißa <leissa@uni-mannheim.de>
2023-07-19 02:11:11 +0200

    wip: #244

--- dialects/affine/passes/lower_for.cpp
+++ dialects/affine/passes/lower_for.cpp
@@ -14,77 +14,34 @@ Ref LowerFor::rewrite(Ref def) {
     if (auto for_ax = match<affine::For>(def)) {
         auto& w = world();
         w.DLOG("rewriting for axiom: {} within {}", for_ax, curr_mut());
-
         auto [begin, end, step, init, body, exit] = for_ax->args<6>();
-        auto body_lam                             = body->isa_mut<Lam>();
-        auto exit_lam                             = exit->isa_mut<Lam>();
+
+        auto body_lam = body->isa_mut<Lam>();
+        auto exit_lam = exit->isa_mut<Lam>();
         if (!body_lam || !exit_lam) return def;
 
-        auto [iter, acc, yield] = body_lam->vars<3>();
+        auto head_cn    = world().cn(merge_sigma(begin->type(), init->type()->projs()));
+        auto head_lam   = world().mut_lam(head_cn)->set("head");
+        auto phis       = head_lam->vars();
+        auto iter       = phis.front();
+        auto accs       = phis.skip_front();
+        auto acc        = world().tuple(accs);
+        auto mem_phi    = mem::mem_var(head_lam);
+        auto bb_type    = world().cn(mem_phi ? mem_phi->type() : world().sigma());
+        auto new_body   = world().mut_lam(bb_type)->set("body_");
+        auto new_exit   = world().mut_lam(bb_type)->set("exit_");
+        auto new_yield  = world().mut_lam(world().cn(init->type()))->set("yield_");
+        iter            = w.call(core::wrap::add, core::Mode::nusw, Defs{iter, step});
+        auto cmp        = w.call(core::icmp::ul, Defs{iter, end});
+        auto iter_acc   = merge_tuple(iter, accs);
+        auto begin_init = merge_tuple(begin, init->projs());
 
-        auto head_lam = world().mut_lam(merge_sigma(iter->type(), world().cn(init->type())->projs()));
-        auto mem_phi  = mem::mem_var(head_lam)->set("mem_phi");
-        auto bb_type  = world().cn(mem_phi ? mem_phi->type() : world().sigma());
-        auto new_body = world().mut_lam(bb_type);
-        auto new_exit = world().mut_lam(bb_type);
-        auto add      = w.call(core::wrap::add, 0_n, Defs{iter, step});
-        auto cmp      = w.call(core::icmp::ul, Defs{iter, end});
         head_lam->branch(false, cmp, new_body, new_exit, mem_phi);
+        new_exit->set(false, exit->reduce(acc).back());
+        new_yield->app(false, head_lam, iter_acc);
+        new_body->set(false, body->reduce(world().tuple({iter, acc, new_yield})).back());
 
-        new_body->set(false, body->reduce(world().tuple(iter,
-
-
-
-
-        auto for_pi = for_ax->callee_type();
-        DefArray for_dom{for_pi->num_doms() - 2, [&](size_t i) {
-            return for_pi->dom(i); }};
-        auto for_lam = w.mut_lam(w.cn(for_dom))->set("for");
-
-        auto body = for_ax->arg(for_ax->num_args() - 2)->set("body");
-        auto exit = for_ax->arg(for_ax->num_args() - 1)->set("exit");
-
-        auto mem_exit = mem::mem_var(exit);
-
-
-
-        //auto eta_boddy = world().mut_lam(
-
-        auto body_type = body->type()->as<Pi>();
-        auto yield_pi  = body_type->doms().back()->as<Pi>();
-        auto yield_lam = w.mut_lam(yield_pi)->set("yield");
-
-        { // construct yield
-            auto [iter, end, step, acc] = for_lam->vars<4>();
-            iter->set("iter");
-            end->set("end");
-            step->set("step");
-            acc->set("acc");
-            auto yield_acc = yield_lam->var();
-
-            auto add = w.call(core::wrap::add, 0_n, Defs{iter, step});
-            yield_lam->app(false, for_lam, {add, end, step, yield_acc});
-        }
-        { // construct for
-            auto [iter, end, step, acc] = for_lam->vars<4>();
-
-            // reduce the body to remove the cn parameter
-            auto mut_body = body->as_mut<Lam>();
-            auto new_body = mut_body->stub(w, w.cn(acc->type()))->set(body->dbg());
-            new_body->set(mut_body->reduce(w.tuple({iter, new_body->var(), yield_lam})));
-
-            // exit
-            auto if_else_cn = w.cn(acc->type());
-            auto if_else    = w.mut_lam(if_else_cn);
-            if_else->app(false, exit if_else->var());
-
-            auto cmp = w.call(core::icmp::ul, Defs{iter, end});
-            for_lam->branch(false, cmp, new_body, if_else, acc);
-        }
-
-        DefArray for_args{for_ax->num_args() - 2, [&](size_t i) {
-            return for_ax->arg(i); }};
-        return rewritten_[def] = w.app(for_lam, for_args);
+        return rewritten_[def] = world().app(head_lam, begin_init);
     }
 
     return def;
--- dialects/affine/passes/lower_for.h
+++ dialects/affine/passes/lower_for.h
@@ -6,6 +6,23 @@
 namespace thorin::affine {
 
 /// Lowers the for axiom to actual control flow in CPS.
+/// ```
+/// .con body(iter: %core.I8, (acc0: .Nat, acc1: .Bool), yield: .Cn [.Nat, .Bool]) = yield (new_acc0, new_acc1);
+/// .con exit(out0: .Nat, out1: .Bool) = use(out0, out1);
+/// %affine.For (256, 2, (.Nat, .Bool)) (begin, end, step, (0, .ff), body, exit)
+/// ```
+/// =>
+/// ```
+/// .con head(iter: %core.I8, acc0: .Nat, acc1: .Bool) =
+///     .let add = %core.wrap.add %core.nusw (iter, step);
+///     .let cmp = %core.icmp.ul (iter, end);
+///     .con new_body() = head (add, new_acc0, new_acc1);
+///     .con new_exit() = use (new_acc0, new_acc1);
+///     (new_exit, new_body)#cmp ()
+/// head (begin, 0, .ff)
+/// ```
+/// * We have to merge `init`/`acc` into the signature, as it may contain a `mem`.
+/// * In this case we have to equip `new_body`/`new_exit` with a `mem` as well.
 class LowerFor : public RWPass<LowerFor, Lam> {
 public:
     LowerFor(PassMan& man)
