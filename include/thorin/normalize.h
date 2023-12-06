c55098f6b3b16f6325e1f7bdec69e4f96c550f9c
Roland Leißa <leissa@uni-mannheim.de>
2023-12-06 15:23:27 +0100

    missing file

---
+++ include/thorin/normalize.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <thorin/util/util.h>
+
+namespace thorin {
+
+class Res {
+public:
+    Res()
+        : data_{} {}
+    template<class T>
+    Res(T val)
+        : data_(thorin::bitcast<u64>(val)) {}
+
+    constexpr const u64& operator*() const& { return *data_; }
+    constexpr u64& operator*() & { return *data_; }
+    explicit operator bool() const { return data_.has_value(); }
+
+private:
+    std::optional<u64> data_;
+};
+
+} // namespace thorin
