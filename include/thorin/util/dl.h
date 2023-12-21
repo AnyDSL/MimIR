d91a2cdeb3cc39895a5423f4f976dc6d9d3e0570
Roland Leißa <leissa@uni-mannheim.de>
2023-12-21 23:16:40 +0100

    fine-tune dl::extensions

--- include/thorin/util/dl.h
+++ include/thorin/util/dl.h
@@ -5,7 +5,15 @@
 
 namespace thorin::dl {
 
-static constexpr auto extensions = {"dll", "dylib", "so"};
+static constexpr auto extensions = {
+#ifdef _WIN32
+    "dll" // Windows
+#elif defined(__APPLE__) && defined(__MACH_)
+    "so", "dylib", // MacOs
+#else
+    "so" // Linux, etc
+#endif
+};
 
 void* open(const char* filename);
 void* get(void* handle, const char* symbol_name);
