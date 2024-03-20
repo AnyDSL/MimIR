52cbd6f917445ab35fb58e1f0ea561d8be1cc272
Roland Leißa <leissa@uni-mannheim.de>
2024-03-20 19:42:10 +0100

    docs

--- include/thorin/def.h
+++ include/thorin/def.h
@@ -414,7 +414,7 @@ public:
 
     /// @name Free Vars and Muts
     ///@{
-    /// * local_muts()/local_vars() are Var%s/Mut%s reachable by following *immutable* extended_ops().
+    /// * local_muts()/local_vars() are Var%s/mutables reachable by following *immutable* extended_ops().
     /// * local_muts()/local_vars() are cached and hash-consed.
     /// * free_vars() compute a global solution, i.e., by transitively following *mutables* as well.
     /// * free_vars() are computed on demand and cached.
