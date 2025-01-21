20cb3efef2354222ed5b6cc47643369a4e482bc2
Roland Leißa <leissa@uni-mannheim.de>
2025-01-21 20:21:15 +0100

    tensor: a * 0 -> 0 working

--- include/mim/plug/tensor/tensor.h
+++ include/mim/plug/tensor/tensor.h
@@ -1,3 +1,20 @@
 #pragma once
 
+#include <mim/world.h>
+
 #include "mim/plug/tensor/autogen.h"
+
+namespace mim::plug::tensor {
+
+inline bool is_zero(Ref zero, Ref def) {
+    if (zero == def) return true;
+    if (auto pack = def->isa<Pack>()) return is_zero(zero, pack->body());
+    return false;
+}
+
+inline Ref mk_zero(Ref zero, nat_t n, nat_t m) {
+    auto& world = zero->world();
+    return world.pack({n, m}, zero);
+}
+
+} // namespace mim::plug::tensor
--- src/mim/plug/tensor/normalizers.cpp
+++ src/mim/plug/tensor/normalizers.cpp
@@ -4,9 +4,18 @@
 
 namespace mim::plug::tensor {
 
-Ref normalize_const(Ref type, Ref, Ref arg) {
+Ref normalize_matmul(Ref type, Ref callee, Ref arg) {
     auto& world = type->world();
-    return world.lit(world.type_idx(arg), 42);
+    auto [a, b] = arg->projs<2>();
+
+    if (auto app = callee->isa<App>()) {
+        auto [l, m, n]           = app->args<3>([](auto i) { return Lit::isa(i); });
+        auto [T, zero, add, mul] = app->decurry()->args<4>();
+
+        if (is_zero(zero, a) || is_zero(zero, b)) return mk_zero(zero, *l, *n);
+    }
+
+    return world.raw_app(type, callee, arg);
 }
 
 MIM_tensor_NORMALIZER_IMPL
