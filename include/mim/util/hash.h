60a69e429488506d33bd7ba8ab0f04404bcddc67
Roland Leißa <leissa@uni-mannheim.de>
2025-02-12 22:51:06 +0100

    use UINT32_C/UINT64_C to be on the safe side

--- include/mim/util/hash.h
+++ include/mim/util/hash.h
@@ -12,20 +12,20 @@ namespace mim {
 ///@{
 /// Use for a single value to hash.
 inline uint32_t murmur3(uint32_t h) {
-    h ^= h >> 16;
-    h *= 0x85ebca6b;
-    h ^= h >> 13;
-    h *= 0xc2b2ae35;
-    h ^= h >> 16;
+    h ^= h >> UINT32_C(16);
+    h *= UINT32_C(0x85ebca6b);
+    h ^= h >> UINT32_C(13);
+    h *= UINT32_C(0xc2b2ae35);
+    h ^= h >> UINT32_C(16);
     return h;
 }
 
 inline uint64_t splitmix64(uint64_t h) {
-    h ^= h >> 30;
-    h *= 0xbf58476d1ce4e5b9;
-    h ^= h >> 27;
-    h *= 0x94d049bb133111eb;
-    h ^= h >> 31;
+    h ^= h >> UINT64_C(30);
+    h *= UINT64_C(0xbf58476d1ce4e5b9);
+    h ^= h >> UINT64_C(27);
+    h *= UINT64_C(0x94d049bb133111eb);
+    h ^= h >> UINT64_C(31);
     return h;
 }
 
@@ -46,13 +46,13 @@ inline size_t hash(size_t h) {
 template<size_t> struct FNV1 {};
 
 template<> struct FNV1<4> {
-    static const uint32_t offset = 2166136261;
-    static const uint32_t prime  = 16777619;
+    static const uint32_t offset = UINT32_C(2166136261);
+    static const uint32_t prime  = UINT32_C(16777619);
 };
 
 template<> struct FNV1<8> {
-    static const uint64_t offset = 14695981039346656037ULL;
-    static const uint64_t prime  = 1099511628211ULL;
+    static const uint64_t offset = UINT64_C(14695981039346656037);
+    static const uint64_t prime  = UINT64_C(1099511628211);
 };
 
 template<class T> size_t hash_combine(size_t seed, T v) {
