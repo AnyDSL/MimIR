f1f5d5ec4f40d3de669f905458bcbc0561f802a2
leissa <leissa@users.noreply.github.com>
2025-02-15 12:06:24 +0000

    deploy: a3b93bff5cfd097e567a4938690fb70978bbbcee

--- classmim_1_1Infer.js
+++ classmim_1_1Infer.js
@@ -1,6 +1,5 @@
 var classmim_1_1Infer =
 [
-    [ "explode", "classmim_1_1Infer.html#aa2cf0a640cdd5459a130cfe55a867e0a", null ],
     [ "find", "classmim_1_1Infer.html#a5f53b044ac29ab53dd69b8c38f9aa393", null ],
     [ "op", "classmim_1_1Infer.html#a92c3c77a9610d3d9c1351a493b03090c", null ],
     [ "rebuild_", "classmim_1_1Infer.html#af47a3129a7b43ccaa5f38360b05c139e", null ],
@@ -8,6 +7,7 @@ var classmim_1_1Infer =
     [ "set", "classmim_1_1Infer.html#ae4228a1ed0081f6822a95acac53c8183", null ],
     [ "stub", "classmim_1_1Infer.html#a22a5c26601676dd2e687fb2995a53aed", null ],
     [ "stub_", "classmim_1_1Infer.html#a17b6f9b1e5f189b368d5d59e8fd4a873", null ],
+    [ "tuplefy", "classmim_1_1Infer.html#a1036cfdaf6556bf7019aca17792785fd", null ],
     [ "unset", "classmim_1_1Infer.html#a9cb038d695aac60c1e4f10000c2807cb", null ],
     [ "Checker", "classmim_1_1Infer.html#a42e74b7760f36154466246901e25398f", null ],
     [ "World", "classmim_1_1Infer.html#a7b4bcdf992c21ae83363f25df05b1d25", null ],
