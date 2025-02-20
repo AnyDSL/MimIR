edec734a3de79876dc8efb946cd0524ae91f2b1f
leissa <leissa@users.noreply.github.com>
2025-02-20 16:54:13 +0000

    deploy: 7771c2c090af4c6e9f68e2702cbc1da280302eb4

--- classmim_1_1ast_1_1IdPtrn.js
+++ classmim_1_1ast_1_1IdPtrn.js
@@ -5,8 +5,8 @@ var classmim_1_1ast_1_1IdPtrn =
     [ "dbg", "classmim_1_1ast_1_1IdPtrn.html#acd85e7188543f166c4da648f5a8beff0", null ],
     [ "emit_type", "classmim_1_1ast_1_1IdPtrn.html#aac311babb4dfb433d4c12c0bd877deb2", null ],
     [ "emit_value", "classmim_1_1ast_1_1IdPtrn.html#af5790cffa55dbd2dab68c0e67cfb6eb8", null ],
-    [ "mk_id", "classmim_1_1ast_1_1IdPtrn.html#a2e0674197a5a6d615a8b4b5a812b0526", null ],
-    [ "mk_type", "classmim_1_1ast_1_1IdPtrn.html#a395c51b65c774bc6e660e880b5e7d52d", null ],
+    [ "make_id", "classmim_1_1ast_1_1IdPtrn.html#a840de34d3af2a12ece8a6e8b96354149", null ],
+    [ "make_type", "classmim_1_1ast_1_1IdPtrn.html#a4532fd0819c1d193a252563c0496ac09", null ],
     [ "stream", "classmim_1_1ast_1_1IdPtrn.html#a935bffb9dba657faf6a4cdda0ae38414", null ],
     [ "type", "classmim_1_1ast_1_1IdPtrn.html#aeba3daa186e84e2f857bcc3e40e8a2fc", null ]
 ];
