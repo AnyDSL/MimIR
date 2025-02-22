4be5d2e3394aa792f3e9d4f06a7f40ab2a001f1e
Roland Leißa <leissa@uni-mannheim.de>
2025-02-22 20:46:06 +0100

    missing files

---
+++ src/mim/analyses/nest.cpp
@@ -0,0 +1,64 @@
+#include "mim/analyses/nest.h"
+
+#include "mim/world.h"
+
+namespace mim {
+
+Nest::Nest(Def* r)
+    : world_(r->world())
+    , root_(make_node(r)) {
+    std::queue<Def*> queue;
+    queue.push(r);
+
+    while (!queue.empty()) {
+        auto curr = pop(queue);
+        auto node = nodes_.find(curr)->second.get();
+        for (auto op : curr->extended_ops()) {
+            for (auto mut : op->local_muts()) {
+                if (!(*this)[mut]) {
+                    if (find_parent(mut, node)) queue.push(mut);
+                }
+            }
+        }
+    }
+}
+
+Nest::Node* Nest::make_node(Def* mut, Node* parent) {
+    auto node = std::make_unique<Node>(mut, parent);
+    auto res  = node.get();
+    nodes_.emplace(mut, std::move(node));
+    return res;
+}
+
+/// Tries to place @p mut as high as possible.
+Nest::Node* Nest::find_parent(Def* mut, Node* begin) {
+    for (Node* node = begin; node; node = node->parent()) {
+        if (auto var = node->mut()->has_var()) {
+            if (mut->free_vars().contains(var)) return make_node(mut, node);
+        }
+    }
+
+    return nullptr;
+}
+
+Vars Nest::vars() const {
+    if (!vars_) {
+        auto vec = Vector<const Var*>();
+        vec.reserve(num_nodes());
+        for (const auto& [mut, _] : nodes_)
+            if (auto var = mut->has_var()) vec.emplace_back(var);
+        vars_ = world().vars().create(vec.begin(), vec.end());
+    }
+
+    return vars_;
+}
+
+void Nest::dump_vars() const {
+    for (auto sep = ""; auto var : vars()) {
+        std::cout << sep << var->gid();
+        sep = ", ";
+    }
+    std::cout << std::endl;
+}
+
+} // namespace mim
