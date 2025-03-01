bae892e4ddbb4bb41486af938cbc944d1db67b74
Roland Leißa <leissa@uni-mannheim.de>
2025-03-01 23:48:35 +0100

    bug fixes
    * cmake: use cmake_dependent_option to prevent MIM_ENABLE_CHECKS for
      Release build
    * correctly static_assert for sizeof(Def) in NDEBUG

--- include/mim/def.h
+++ include/mim/def.h
@@ -606,7 +606,11 @@ private:
     friend std::ostream& operator<<(std::ostream&, const Def*);
 };
 
+#ifdef NDEBUG
+static_assert(sizeof(void*) != 8 || sizeof(Def) == 12 * 8, "make sure to not accidently increase sizeof(Def)");
+#else
 static_assert(sizeof(void*) != 8 || sizeof(Def) == 13 * 8, "make sure to not accidently increase sizeof(Def)");
+#endif
 
 /// @name DefDef
 ///@{
