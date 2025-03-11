78ffe4ac9a7af1eb019d34c413f950bd580b3d96
Roland Leißa <leissa@uni-mannheim.de>
2025-03-11 02:34:49 +0100

    Trie: missing swaps

--- include/mim/util/trie.h
+++ include/mim/util/trie.h
@@ -222,7 +222,9 @@ public:
     friend void swap(Trie& t1, Trie& t2) noexcept {
         using std::swap;
         swap(t1.arena_, t2.arena_);
+        swap(t1.size_, t2.size_);
         swap(t1.root_, t2.root_);
+        swap(t1.counter_, t2.counter_);
     }
 
 private:
