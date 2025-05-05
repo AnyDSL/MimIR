e29535df6ed8ad0bbd1e6bed2527c1d02683aef0
Roland Leißa <leissa@uni-mannheim.de>
2025-05-06 00:44:06 +0200

    python module is now named "mim"

--- src/mim/py/py.cpp
+++ src/mim/py/py.cpp
@@ -7,7 +7,7 @@ void init_world(py::module_&);
 void init_driver(py::module_&);
 } // namespace mim
 
-PYBIND11_MODULE(pymim, m) {
+PYBIND11_MODULE(mim, m) {
     mim::init_driver(m);
     mim::init_world(m);
 }
