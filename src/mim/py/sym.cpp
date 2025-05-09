7d88c2da8f9d1ed2165bc1d8c86165928f153cf3
Roland Leißa <leissa@uni-mannheim.de>
2025-05-09 22:59:58 +0200

    fixing undefined reference

--- src/mim/py/sym.cpp
+++ src/mim/py/sym.cpp
@@ -1,16 +1,15 @@
-#include <pybind11/pybind11.h>
-
 #include <fe/sym.h>
+#include <pybind11/pybind11.h>
 
 namespace py = pybind11;
 
-namespace nim {
+namespace mim {
 
 void init_sym(py::module_& m) {
     py::class_<fe::Sym>(m, "Sym")
-        .def(py::init<>());
-        // .def("empty", &fe::Sym::empty, py::return_value_policy::reference)
-        // .def("size", &fe::Sym::size, py::return_value_policy::reference);
+        .def(py::init<>())
+        .def("empty", &fe::Sym::empty, py::return_value_policy::reference)
+        .def("size", &fe::Sym::size, py::return_value_policy::reference);
 }
 
 } // namespace mim
