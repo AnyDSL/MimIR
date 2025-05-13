b40e325b6db2d645d4f99ec221b260d65d4b8b5d
Roland Leißa <leissa@uni-mannheim.de>
2025-05-13 07:40:35 +0200

    docs + move &core.zip -> %tuple.zip

--- src/mim/plug/tuple/normalizers.cpp
+++ src/mim/plug/tuple/normalizers.cpp
@@ -20,6 +20,47 @@ const Def* normalize_concat(const Def* type, const Def* callee, const Def* arg) 
     return nullptr;
 }
 
+const Def* normalize_zip(const Def* type, const Def* c, const Def* arg) {
+    auto& w                    = type->world();
+    auto callee                = c->as<App>();
+    auto is_os                 = callee->arg();
+    auto [n_i, Is, n_o, Os, f] = is_os->projs<5>();
+    auto [r, s]                = callee->decurry()->args<2>();
+    auto lr                    = Lit::isa(r);
+    auto ls                    = Lit::isa(s);
+
+    // TODO commute
+    // TODO reassociate
+    // TODO more than one Os
+    // TODO select which Is/Os to zip
+
+    if (lr && ls && *lr == 1 && *ls == 1) return w.app(f, arg);
+
+    if (auto l_in = Lit::isa(n_i)) {
+        auto args = arg->projs(*l_in);
+
+        if (lr && std::ranges::all_of(args, [](const Def* arg) { return arg->isa<Tuple, Pack>(); })) {
+            auto shapes = s->projs(*lr);
+            auto s_n    = Lit::isa(shapes.front());
+
+            if (s_n) {
+                auto elems = DefVec(*s_n, [&, f = f](size_t s_i) {
+                    auto inner_args = DefVec(args.size(), [&](size_t i) { return args[i]->proj(*s_n, s_i); });
+                    if (*lr == 1) {
+                        return w.app(f, inner_args);
+                    } else {
+                        auto app_zip = w.app(w.annex<zip>(), {w.lit_nat(*lr - 1), w.tuple(shapes.view().subspan(1))});
+                        return w.app(w.app(app_zip, is_os), inner_args);
+                    }
+                });
+                return w.tuple(elems);
+            }
+        }
+    }
+
+    return {};
+}
+
 MIM_tuple_NORMALIZER_IMPL
 
 } // namespace mim::plug::tuple
