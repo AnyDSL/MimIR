6c62844ec85ef9ba2e8817b4e3d1774da8f85b15
Roland Leißa <leissa@uni-mannheim.de>
2025-05-15 19:13:41 +0200

    new vec plugin
    * fold.l + fold.r

---
+++ include/mim/plug/vec/vec.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "mim/plug/vec/autogen.h"
---
+++ src/mim/plug/vec/normalizers.cpp
@@ -0,0 +1,28 @@
+#include "mim/world.h"
+
+#include "mim/plug/vec/vec.h"
+
+namespace mim::plug::vec {
+
+template<fold id> const Def* normalize_fold(const Def*, const Def* c, const Def* arg) {
+    auto& w     = c->world();
+    auto callee = c->as<App>();
+    auto f      = callee->arg();
+
+    auto [acc, vec] = arg->projs<2>();
+    if constexpr (id == fold::r) std::swap(acc, vec);
+
+    if (auto tuple = vec->isa<Tuple>()) {
+        if constexpr (id == fold::l)
+            for (auto op : tuple->ops()) acc = w.app(f, {acc, op});
+        else // fold::r
+            for (auto op : tuple->ops() | std::ranges::views::reverse) acc = w.app(f, {op, acc});
+        return acc;
+    }
+
+    return nullptr;
+}
+
+MIM_vec_NORMALIZER_IMPL
+
+} // namespace mim::plug::vec
