0d642b88544051a73acb358fb758b52aa389c4e3
Roland Leißa <leissa@uni-mannheim.de>
2025-05-19 01:27:55 +0200

    fixed header

--- src/mim/pass/pass.cpp
+++ src/mim/pass/pass.cpp
@@ -1,9 +1,9 @@
 #include "mim/pass/pass.h"
 
+#include <absl/container/fixed_array.h>
+
 #include "mim/phase/phase.h"
 #include "mim/util/util.h"
-
-#include "absl/container/fixed_array.h"
 
 namespace mim {
 
