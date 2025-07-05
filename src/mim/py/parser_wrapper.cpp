e4c6523c98bdc5caa2a1898bfa75056432d88503
Roland Leißa <leissa@uni-mannheim.de>
2025-07-05 21:28:57 +0200

    use AST from parser

--- src/mim/py/parser_wrapper.cpp
+++ src/mim/py/parser_wrapper.cpp
@@ -1,19 +1,21 @@
 #include <mim/def.h>
 
 #include <mim/ast/parser.h>
+
 namespace mim::ast {
 class PyParser {
 public:
     PyParser(Parser* parser)
         : parser_(parser) {}
 
-    void plugin(const std::string& plug) {
-        auto ast = ast::AST(parser_->driver().world());
-        auto mod = parser_->plugin(plug);
+    void plugin(std::string_view plug) {
+        auto& ast = parser_->ast();
+        auto mod  = parser_->import(plug);
         mod->compile(ast);
     }
 
 private:
     Parser* parser_;
 };
+
 } // namespace mim::ast
