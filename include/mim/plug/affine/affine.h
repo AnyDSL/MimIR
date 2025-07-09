391377eef155cd92644299006b02b7e0e3674457
Joachim Meyer <jmeyer@cs.uni-saarland.de>
2025-07-09 10:37:43 +0200

    [Affine] Infer type arguments for `affine.For`.

--- include/mim/plug/affine/affine.h
+++ include/mim/plug/affine/affine.h
@@ -6,20 +6,11 @@
 
 namespace mim::plug::affine {
 
-/// @name %%affine.For
-/// Returns the affine_for axm applied with \a params.
-/// See documentation for %affine.For axm in @ref affine.
-///@{
-inline const Def* fn_for(World& w, Defs params) {
-    return w.app(w.annex<affine::For>(), {w.lit_i32(), w.lit_nat(params.size()), w.tuple(params)});
-}
-
 /// Returns a fully applied affine_for axm.
 /// See documentation for %affine.For axm in @ref affine.
 inline const Def*
 op_for(World& w, const Def* begin, const Def* end, const Def* step, Defs inits, const Def* body, const Def* brk) {
-    auto types = DefVec(inits.size(), [&](size_t i) { return inits[i]->type(); });
-    return w.app(fn_for(w, types), {begin, end, step, w.tuple(inits), body, brk});
+    return w.app(w.annex<affine::For>(), {begin, end, step, w.tuple(inits), body, brk});
 }
 ///@}
 
