dbebd516473c7f8b55e9c40a876b5b804d0b8fc4
Joachim Meyer <jmeyer@cs.uni-saarland.de>
2025-07-09 11:22:40 +0200

    Also remove `op_for` in favor of `call<affine::For>`.

--- include/mim/plug/affine/affine.h
+++ include/mim/plug/affine/affine.h
@@ -1,17 +1,3 @@
 #pragma once
 
-#include "mim/world.h"
-
 #include "mim/plug/affine/autogen.h"
-
-namespace mim::plug::affine {
-
-/// Returns a fully applied affine_for axm.
-/// See documentation for %affine.For axm in @ref affine.
-inline const Def*
-op_for(World& w, const Def* begin, const Def* end, const Def* step, Defs inits, const Def* body, const Def* brk) {
-    return w.app(w.annex<affine::For>(), {begin, end, step, w.tuple(inits), body, brk});
-}
-///@}
-
-} // namespace mim::plug::affine
