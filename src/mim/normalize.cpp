4974883dacb8a496aedb88e665ccdc51a90c85e2
Roland Leißa <leissa@uni-mannheim.de>
2025-07-24 20:59:56 +0200

    fine-tune commute

--- src/mim/normalize.cpp
+++ src/mim/normalize.cpp
@@ -13,6 +13,12 @@ namespace mim {
     if (a->isa_imm() && b->isa_mut()) return -1;
     if (a->isa_mut() && b->isa_imm()) return +1;
 
+    // clang-format off
+    if (a->node()    != b->node()   ) return a->node()    < b->node()    ? -1 : +1;
+    if (a->num_ops() != b->num_ops()) return a->num_ops() < b->num_ops() ? -1 : +1;
+    if (a->flags()   != b->flags()  ) return a->flags()   < b->flags()   ? -1 : +1;
+    // clang-format on
+
     if (a->isa_mut() && b->isa_mut()) {
         world.WLOG("resorting to unstable gid-based compare for commute check");
         return a->gid() < b->gid() ? -1 : +1;
@@ -20,20 +26,17 @@ namespace mim {
 
     assert(a->isa_imm() && b->isa_imm());
 
-    // clang-format off
-    if (a->node()    != b->node()   ) return a->node()    < b->node()    ? -1 : +1;
-    if (a->num_ops() != b->num_ops()) return a->num_ops() < b->num_ops() ? -1 : +1;
-    if (a->flags()   != b->flags()  ) return a->flags()   < b->flags()   ? -1 : +1;
-    // clang-format on
-
     if (auto var1 = a->isa<Var>()) {
         auto var2 = b->as<Var>();
         world.WLOG("resorting to unstable gid-based compare for commute check");
         return var1->gid() < var2->gid() ? -1 : +1;
     }
 
-    for (size_t i = 0, e = a->num_ops(); i != e; ++i)
+    // heuristic: iterate backwards as index (often a Lit) comes last and will faster find a solution
+    for (size_t i = a->num_ops(); i-- != 0;)
         if (int cmp = commute_(a->op(i), b->op(i)); cmp != 0) return cmp;
+
+    if (int cmp = commute_(a->type(), b->type()); cmp != 0) return cmp;
 
     fe::unreachable();
 }
