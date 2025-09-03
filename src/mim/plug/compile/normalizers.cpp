1305305fcd6c80dade8f0510eae29cf3f496ee11
Roland Leißa <leissa@uni-mannheim.de>
2025-09-03 16:07:50 +0200

    removed unused normalizer

--- src/mim/plug/compile/normalizers.cpp
+++ src/mim/plug/compile/normalizers.cpp
@@ -13,26 +13,6 @@ const Def* normalize_is_loaded(const Def*, const Def*, const Def* arg) {
     return {};
 }
 
-const Def* normalize_cat(const Def* type, const Def* callee, const Def* arg) {
-    auto& world = type->world();
-    auto [a, b] = arg->projs<2>();
-    auto [n, m] = callee->as<App>()->args<2>([](auto def) { return Lit::isa(def); });
-
-    if (n && *n == 0) return b;
-    if (m && *m == 0) return a;
-
-    if (n && m) {
-        auto defs = DefVec();
-        for (size_t i = 0, e = *n; i != e; ++i)
-            defs.emplace_back(a->proj(e, i));
-        for (size_t i = 0, e = *m; i != e; ++i)
-            defs.emplace_back(b->proj(e, i));
-        return world.tuple(defs);
-    }
-
-    return nullptr;
-}
-
 MIM_compile_NORMALIZER_IMPL
 
 } // namespace mim::plug::compile
