3d61a05ffdff0a58eab5a052a4339bd6e088e416
Roland Leißa <leissa@uni-mannheim.de>
2025-09-11 23:13:30 +0200

    affine.For: curry for_body & for_exit

--- src/mim/plug/affine/phase/lower_for.cpp
+++ src/mim/plug/affine/phase/lower_for.cpp
@@ -34,19 +34,15 @@ const Def* merge_t(const Def* elem, const Def* tuple, const Def* mem) {
 const Def* LowerFor::rewrite_imm_App(const App* app) {
     if (auto for_ax = Axm::isa<affine::For>(app)) {
         DLOG("rewriting for axm: `{}`", for_ax);
-        auto [old_begin, old_end, old_step, old_init, old_body, old_exit] = for_ax->args<6>();
+        auto [old_body, old_exit, args]               = for_ax->uncurry_args<3>();
+        auto [new_begin, new_end, new_step, new_init] = args->projs<4>([this](const Def* def) { return rewrite(def); });
 
         auto old_body_lam = old_body->isa_mut<Lam>();
         auto old_exit_lam = old_exit->isa_mut<Lam>();
         if (!old_body_lam) old_body_lam = Lam::eta_expand(old_body);
         if (!old_exit_lam) old_exit_lam = Lam::eta_expand(old_exit);
 
-        auto new_begin = rewrite(old_begin);
-        auto new_end   = rewrite(old_end);
-        auto new_step  = rewrite(old_step);
-        auto new_init  = rewrite(old_init);
-        auto new_mem   = mem::mem_def(new_init);
-
+        auto new_mem      = mem::mem_def(new_init);
         auto new_head_lam = new_world().mut_con(merge_s(new_begin->type(), new_init->type(), new_mem))->set("head");
         auto new_phis     = new_head_lam->vars();
         auto new_iter     = new_phis.front();
