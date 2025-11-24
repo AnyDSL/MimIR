87aab7e197168624d61b6bfe8e5c58ff0e294831
Thilo Wischmeyer <thilo.wischmeyer@stud.uni-goettingen.de>
2025-11-24 19:35:03 +0100

    add check if var of a lam is top before rebuilding
    If we don't check this we rebuild externals in some test cases.

--- src/mim/phase/sccp.cpp
+++ src/mim/phase/sccp.cpp
@@ -155,55 +155,56 @@ const Def* SCCP::join(const Def* var, const Def* abstr) {
 const Def* SCCP::rewrite_imm_App(const App* old_app) {
     if (auto old_lam = old_app->callee()->isa_mut<Lam>(); old_lam && old_lam->is_set()) {
         if (auto var = old_lam->has_var()) {
-            size_t num_old = old_lam->num_vars();
+            if (var != concr2abstr_[var]) {
+                size_t num_old = old_lam->num_vars();
 
-            Lam* new_lam;
-            if (auto i = lam2lam_.find(old_lam); i != lam2lam_.end())
-                new_lam = i->second;
-            else {
-                // build new dom
-                auto new_doms = DefVec();
-                for (size_t i = 0; i != num_old; ++i) {
-                    auto old_var = var->proj(num_old, i);
-                    auto abstr   = concr2abstr_[var->proj(num_old, i)];
-                    if (abstr == old_var) new_doms.emplace_back(rewrite(old_lam->dom(num_old, i)));
+                Lam* new_lam;
+                if (auto i = lam2lam_.find(old_lam); i != lam2lam_.end())
+                    new_lam = i->second;
+                else {
+                    // build new dom
+                    auto new_doms = DefVec();
+                    for (size_t i = 0; i != num_old; ++i) {
+                        auto old_var = var->proj(num_old, i);
+                        auto abstr   = concr2abstr_[var->proj(num_old, i)];
+                        if (abstr == old_var) new_doms.emplace_back(rewrite(old_lam->dom(num_old, i)));
+                    }
+
+                    // build new lam
+                    size_t num_new    = new_doms.size();
+                    auto new_vars     = absl::FixedArray<const Def*>(num_old);
+                    new_lam           = new_world().mut_lam(new_doms, rewrite(old_lam->codom()))->set(old_lam->dbg());
+                    lam2lam_[old_lam] = new_lam;
+
+                    for (size_t i = 0, j = 0; i != num_old; ++i) {
+                        auto old_var = var->proj(num_old, i);
+                        auto abstr   = concr2abstr_[old_var];
+                        if (abstr == old_var)
+                            new_vars[i] = new_lam->var(num_new, j++);
+                        else
+                            new_vars[i] = rewrite(abstr);
+                    }
+                    auto tup = new_world().tuple(new_vars);
+                    map(var, tup);
+
+                    // TODO or below?
+                    new_lam->set(rewrite(old_lam->filter()), rewrite(old_lam->body()));
                 }
 
-                // build new lam
-                size_t num_new    = new_doms.size();
-                auto new_vars     = absl::FixedArray<const Def*>(num_old);
-                new_lam           = new_world().mut_lam(new_doms, rewrite(old_lam->codom()))->set(old_lam->dbg());
-                lam2lam_[old_lam] = new_lam;
-
+                // build new app
+                size_t num_new = new_lam->num_vars();
+                auto new_args  = absl::FixedArray<const Def*>(num_new);
                 for (size_t i = 0, j = 0; i != num_old; ++i) {
                     auto old_var = var->proj(num_old, i);
                     auto abstr   = concr2abstr_[old_var];
-                    if (abstr == old_var)
-                        new_vars[i] = new_lam->var(num_new, j++);
-                    else
-                        new_vars[i] = rewrite(abstr);
+                    if (abstr == old_var) new_args[j++] = rewrite(old_app->arg(num_old, i));
                 }
-                auto tup = new_world().tuple(new_vars);
-                map(var, tup);
 
-                // TODO or below?
-                new_lam->set(rewrite(old_lam->filter()), rewrite(old_lam->body()));
+                auto new_app = new_world().app(new_lam, new_args);
+                map(old_app, new_app);
+                return new_app;
             }
-
-            // build new app
-            size_t num_new = new_lam->num_vars();
-            auto new_args  = absl::FixedArray<const Def*>(num_new);
-            for (size_t i = 0, j = 0; i != num_old; ++i) {
-                auto old_var = var->proj(num_old, i);
-                auto abstr   = concr2abstr_[old_var];
-                if (abstr == old_var) new_args[j++] = rewrite(old_app->arg(num_old, i));
-            }
-
-            auto new_app = new_world().app(new_lam, new_args);
-            map(old_app, new_app);
-            return new_app;
         }
-        // }
     }
 
     return Rewriter::rewrite_imm_App(old_app);
