84133f2cd16af2d676677a700101ce03519f4993
Manuel Lata <contact@mlata.me>
2025-11-28 14:07:08 +0100

    add vulkan plugin

---
+++ src/mim/plug/vulkan/vulkan.cpp
@@ -0,0 +1,11 @@
+#include "mim/plug/vulkan/vulkan.h"
+
+#include <mim/pass.h>
+#include <mim/plugin.h>
+
+using namespace mim;
+
+/// Registers normalizers as well as Phase%s and Pass%es for the Axm%s of this Plugin.
+extern "C" MIM_EXPORT Plugin mim_get_plugin() {
+    return {"vulkan", plug::vulkan::register_normalizers, nullptr, nullptr};
+}
