9b3bf42372e165b3fd3cff797a2551c80d3b7ce9
Thilo Wischmeyer <thilo.wischmeyer@stud.uni-goettingen.de>
2025-12-03 14:47:40 +0100

    first load store simplification works

--- src/mim/plug/mem/phase/ssa_phase.cpp
+++ src/mim/plug/mem/phase/ssa_phase.cpp
@@ -1,6 +1,10 @@
-#include "mim/plug/mem/phase/ssa_phase.h"
-
 #include <absl/container/fixed_array.h>
+
+#include "mim/def.h"
+#include "mim/tuple.h"
+
+#include "mim/plug/mem/autogen.h"
+#include "mim/plug/mem/phase/ssa_phase.h"
 
 namespace mim::plug::mem::phase {
 
@@ -14,6 +18,12 @@ bool SSAPhase::analyze() {
             concr2abstr(init(def));
         for (auto def : old_world().externals().muts())
             concr2abstr(init(def));
+    }
+
+    for (auto [k, v] : concr2abstr_) {
+        if (auto p = v->isa<Proxy>()) {
+            outln("{}: {, }", k, p->ops());
+        }
     }
 
     return false; // no fixed-point neccessary
@@ -46,6 +56,39 @@ const Def* SSAPhase::concr2abstr(const Def* concr) {
     return concr2abstr(concr, concr2abstr_impl(concr)).first;
 }
 
+const Def* mem_store(const Def* mem, const Def* key, const Def* value) {
+    auto& world    = mem->world();
+    auto new_entry = world.tuple({key, value});
+    if (auto proxy = mem->isa<Proxy>()) {
+        auto new_map_entries = DefVec();
+        new_map_entries.push_back(mem);
+        for (auto kv : proxy->ops() | std::views::drop(1)) {
+            auto [k, v] = kv->projs<2>();
+            if (k != key) new_map_entries.push_back(kv);
+        }
+        new_map_entries.push_back(new_entry);
+        return world.proxy(world.annex<mem::M>(), new_map_entries, 0, 0);
+    }
+    return world.proxy(world.annex<mem::M>(), {mem, new_entry}, 0, 0);
+}
+
+std::pair<const Def*, const Def*> mem_load(const Def* mem, const Def* key) {
+    if (auto proxy = mem->isa<Proxy>()) {
+        for (auto kv : proxy->ops() | std::views::drop(1)) {
+            auto [k, v] = kv->projs<2>();
+            if (k == key) return {proxy->op(0), v};
+        }
+    }
+    return {mem, nullptr};
+}
+
+const App* get_slot(const Def* ptr) {
+    if (auto extract = ptr->isa<Extract>()) {
+        return Axm::isa<mem::slot>(extract->tuple());
+    }
+    return nullptr;
+}
+
 const Def* SSAPhase::concr2abstr_impl(const Def* def) {
     if (auto type = def->type()) concr2abstr(type);
 
@@ -54,6 +97,21 @@ const Def* SSAPhase::concr2abstr_impl(const Def* def) {
         auto l     = Lit::isa<bool>(abstr);
         if (l && *l) return concr2abstr(branch.tt());
         if (l && !*l) return concr2abstr(branch.ff());
+    } else if (auto load = Axm::isa<mem::load>(def)) {
+        auto [mem, ptr] = load->args<2>();
+        if (auto slot = get_slot(ptr)) {
+            auto id  = slot->arg(1);
+            auto map = concr2abstr(mem);
+            if (auto [_, value] = mem_load(map, id); value) return old_world().tuple({map, value});
+        }
+    } else if (auto store = Axm::isa<mem::store>(def)) {
+        auto [mem, ptr, val] = store->args<3>();
+        if (auto slot = get_slot(ptr)) {
+            auto map       = concr2abstr(mem);
+            auto id        = slot->arg(1);
+            auto abstr_val = concr2abstr(val);
+            return mem_store(map, id, abstr_val);
+        }
     } else if (auto app = def->isa<App>()) {
         if (auto lam = app->callee()->isa_mut<Lam>(); lam && lam->is_set()) {
             auto ins = concr2abstr(lam, lam).second;
@@ -160,6 +218,21 @@ const Def* SSAPhase::rewrite_imm_App(const App* old_app) {
 
             return map(old_app, new_world().app(new_lam, new_args));
         }
+    } else if (auto load = Axm::isa<mem::load>(old_app)) {
+        auto [mem, ptr] = load->args<2>();
+        if (auto slot = get_slot(ptr)) {
+            auto id  = slot->arg(1);
+            auto map = concr2abstr(mem);
+            if (auto [mem, value] = mem_load(map, id); value) return new_world().tuple({rewrite(mem), rewrite(value)});
+        }
+    } else if (auto store = Axm::isa<mem::store>(old_app)) {
+        // auto [mem, ptr, val] = store->args<3>();
+        // if (auto slot = get_slot(ptr)) {
+        //     auto map = concr2abstr(mem);
+        //     auto id = slot->arg(1);
+        //     auto abstr_val = concr2abstr(val);
+        //     return map_insert(map, id, abstr_val);
+        // }
     }
 
     return Rewriter::rewrite_imm_App(old_app);
