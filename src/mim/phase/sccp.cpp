2283f6c46a87640c2af7bbdb2151aa6a2b15ce3c
Roland Leißa <leissa@uni-mannheim.de>
2025-12-03 11:29:12 +0100

    sccp: added tests
    * sccp/loop.mim still relies on PassMgr to work

--- src/mim/phase/sccp.cpp
+++ src/mim/phase/sccp.cpp
@@ -49,13 +49,12 @@ const Def* SCCP::concr2abstr(const Def* concr) {
 const Def* SCCP::concr2abstr_impl(const Def* def) {
     if (auto type = def->type()) concr2abstr(type);
 
-    // if (auto branch = Branch(def)) {
-    //     auto abstr = branch.cond();
-    //     auto l     = Lit::isa<bool>(abstr);
-    //     if (l && *l) concr2abstr(branch.tt());
-    //     if (l && !*l) concr2abstr(branch.ff());
-    /*} else*/
-    if (auto app = def->isa<App>()) {
+    if (auto branch = Branch(def)) {
+        auto abstr = branch.cond();
+        auto l     = Lit::isa<bool>(abstr);
+        if (l && *l) return concr2abstr(branch.tt());
+        if (l && !*l) return concr2abstr(branch.ff());
+    } else if (auto app = def->isa<App>()) {
         if (auto lam = app->callee()->isa_mut<Lam>(); lam && lam->is_set()) {
             auto ins = concr2abstr(lam, lam).second;
 
