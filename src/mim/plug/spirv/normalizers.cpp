073a232c4cbd3c5fa35abd7c98b7320bd4a6711e
Manuel Lata <contact@mlata.me>
2025-12-09 18:35:14 +0100

    initial emitter impl

--- src/mim/plug/spirv/normalizers.cpp
+++ src/mim/plug/spirv/normalizers.cpp
@@ -4,11 +4,6 @@
 
 namespace mim::plug::spirv {
 
-const Def* normalize_const(const Def* type, const Def*, const Def* arg) {
-    auto& world = type->world();
-    return world.lit(world.type_idx(arg), 42);
-}
-
-MIM_spirv_NORMALIZER_IMPL
+// MIM_spirv_NORMALIZER_IMPL
 
 } // namespace mim::plug::spirv
