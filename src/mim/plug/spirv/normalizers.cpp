737487d7aa0696a777eaa94e304383239b3c8587
Manuel Lata <contact@mlata.me>
2025-12-21 23:00:00 +0100

    spirv refactor (not working yet)

--- src/mim/plug/spirv/normalizers.cpp
+++ src/mim/plug/spirv/normalizers.cpp
@@ -1,9 +1,42 @@
+#include "mim/tuple.h"
 #include "mim/world.h"
 
-#include "mim/plug/spirv/spirv.h"
+#include "mim/util/types.h"
+
+#include "mim/plug/mem/autogen.h"
+#include "mim/plug/spirv/autogen.h"
 
 namespace mim::plug::spirv {
 
 // MIM_spirv_NORMALIZER_IMPL
+
+const Def* normalize_wrap(const Def* type, const Def*, const Def* arg) {
+    auto& world      = type->world();
+    auto [Gi, Go, f] = arg->projs<3>();
+
+    // length has to be known at compile time
+    if (!Gi->isa<Tuple>()) return nullptr;
+
+    // auto name_ = std::format("{}_", f->node_name()
+    auto mem_t = world.call<mem::M>(0);
+    Lam* f_    = world.mut_fun({mem_t, Gi, Go}, mem_t)->set("main");
+
+    auto [mem, gis, gos, ret] = f_->vars<4>();
+
+    std::vector<const Def*> is{};
+    for (auto gi : gis->ops()) {
+        auto [mem_, i] = world.call<spirv::load>(world.tuple({mem, gi}))->projs<2>();
+        mem            = mem_;
+        is.push_back(i);
+    }
+
+    auto res = world.app(f, is)->projs(gos->num_projs());
+    for (nat_t index = 0; index < gos->num_projs(); index++)
+        mem = world.call<spirv::store>(world.tuple({mem, gos->proj(index)}), res[index]);
+    f_->app(false, ret, mem);
+    f_->externalize();
+
+    return f_;
+}
 
 } // namespace mim::plug::spirv
--- src/mim/plug/spirv/spirv.cpp
+++ src/mim/plug/spirv/spirv.cpp
@@ -1,8 +1,7 @@
-#include "mim/plug/spirv/spirv.h"
-
 #include <mim/pass.h>
 #include <mim/plugin.h>
 
+#include "mim/plug/spirv/autogen.h"
 #include "mim/plug/spirv/be/emit.h"
 
 using namespace mim;
@@ -10,5 +9,6 @@ using namespace mim::plug;
 
 /// Registers normalizers as well as Phase%s and Pass%es for the Axm%s of this Plugin.
 extern "C" MIM_EXPORT Plugin mim_get_plugin() {
-    return {"spirv", nullptr, nullptr, [](Backends& backends) { backends["spirv"] = &spirv::emit_asm; }};
+    return {"spirv", plug::spirv::register_normalizers, nullptr,
+            [](Backends& backends) { backends["spirv"] = &spirv::emit_asm; }};
 }
