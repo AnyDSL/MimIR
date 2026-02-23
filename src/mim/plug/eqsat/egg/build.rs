cc469f435e7aaeaca4c32e1c1c7dbbde7cb84035
ashiven <nevisha@pm.me>
2026-02-23 10:49:27 +0100

    fixed egg lib not being linked against debug crt

--- src/mim/plug/eqsat/egg/build.rs
+++ src/mim/plug/eqsat/egg/build.rs
@@ -1,5 +1,13 @@
-fn main() {
-    cxx_build::bridge("src/lib.rs").compile("mimffi");
+use std::env;
 
+fn main() {
+    cxx_build::bridge("src/lib.rs").compile("eqsat");
     println!("cargo:rerun-if-changed=src/lib.rs");
+
+    if env::var("TARGET").is_ok_and(|s| s.contains("windows-msvc"))
+        && env::var("CFLAGS").is_ok_and(|s| s.contains("/MDd"))
+    {
+        println!("cargo::rustc-link-arg=/nodefaultlib:msvcrt");
+        println!("cargo::rustc-link-arg=/defaultlib:msvcrtd");
+    }
 }
