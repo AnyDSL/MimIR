02c54cb4c4f5c17aec06d5473b03c1a78ad995e1
ashiven <nevisha@pm.me>
2026-03-12 11:28:21 +0100

    start implementing eqsat axioms

--- src/mim/plug/eqsat/eqsat.cpp
+++ src/mim/plug/eqsat/eqsat.cpp
@@ -7,13 +7,7 @@
 using namespace mim;
 using namespace mim::plug;
 
-void reg_stages(Flags2Stages& stages) {
-    // clang-format off
-    Stage::hook<eqsat::egg_rewrite_phase, eqsat::EggRewrite>(stages);
-    // clang-format on
-}
+void reg_stages(Flags2Stages& stages) { Stage::hook<eqsat::egg_rewrite_phase, eqsat::EggRewrite>(stages); }
 
 /// Registers normalizers as well as Phase%s and Pass%es for the Axm%s of this Plugin.
-extern "C" MIM_EXPORT Plugin mim_get_plugin() {
-    return {"eqsat", plug::eqsat::register_normalizers, reg_stages, nullptr};
-}
+extern "C" MIM_EXPORT Plugin mim_get_plugin() { return {"eqsat", nullptr, reg_stages, nullptr}; }
