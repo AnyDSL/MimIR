f39f750d89c1dec8d1dd407aa12b57a526ae7def
Manuel Lata <contact@mlata.me>
2026-03-24 19:19:03 +0100

    refactor sflow axioms (again)

--- src/mim/plug/sflow/normalizers.cpp
+++ src/mim/plug/sflow/normalizers.cpp
@@ -9,6 +9,15 @@ const Def* normalize_const(const Def* type, const Def*, const Def* arg) {
     return world.lit(world.type_idx(arg), 42);
 }
 
+const Def* normalize_branch(const Def* type, const Def* callee, const Def* args) {
+    auto& world                            = type->world();
+    auto [n, Ts, merge, tuple, index, arg] = App::uncurry_args<6>(callee);
+
+    if (Lit::as(n) == 1) return world.app(world.extract(tuple, index), arg);
+
+    return world.raw_app(type, callee, args);
+}
+
 MIM_sflow_NORMALIZER_IMPL
 
 } // namespace mim::plug::sflow
