7440488a6ee9af5a45ac1108ba0068cc856c6e81
ashiven <nevisha@pm.me>
2026-04-11 17:06:36 +0200

    add more rebuild test cases

--- src/mim/plug/core/be/sexpr.cpp
+++ src/mim/plug/core/be/sexpr.cpp
@@ -160,9 +160,6 @@ std::string Emitter::convert(const Def* type, const Def* var /*= nullptr*/) {
             }
         }
         print(s, "(idx (lit {}))", size);
-    } else if (auto w = mim::plug::math::isa_f(type)) {
-        // TODO: How do we emit this properly?
-        print(s, "(lit {})", type);
     } else if (auto lit = type->isa<Lit>()) {
         if (lit->type()->isa<Nat>())
             print(s, "(lit {})", lit->get());
